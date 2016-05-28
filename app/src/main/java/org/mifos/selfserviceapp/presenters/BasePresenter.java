cde548adc465979c74fbe03b809bb4923f17bf4b
Ishan Khanna <ishan1604@gmail.com>
2016-05-28 21:45:35 +0530

    chore: Initialised project with MVP Architecture packages

---
+++ app/src/main/java/org/mifos/selfserviceapp/presenters/BasePresenter.java
@@ -0,0 +1,50 @@
+package org.mifos.selfserviceapp.presenters;
+
+import android.content.Context;
+
+import org.mifos.selfserviceapp.ui.views.base.MVPView;
+
+/**
+ * @author ishan
+ * @since 19/05/16
+ */
+public class BasePresenter<T extends MVPView> implements Presenter<T> {
+
+    private T mMvpView;
+
+    protected Context context;
+
+    protected BasePresenter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void attachView(T mvpView) {
+        mMvpView = mvpView;
+    }
+
+    @Override
+    public void detachView() {
+        mMvpView = null;
+    }
+
+    public boolean isViewAttached() {
+        return mMvpView != null;
+    }
+
+    public T getMvpView() {
+        return mMvpView;
+    }
+
+    public void checkViewAttached() {
+        if (!isViewAttached()) throw new MvpViewNotAttachedException();
+    }
+
+    public static class MvpViewNotAttachedException extends RuntimeException {
+        public MvpViewNotAttachedException() {
+            super("Please call Presenter.attachView(MvpView) before" +
+                    " requesting data to the Presenter");
+        }
+    }
+
+}
---
+++ app/src/main/java/org/mifos/selfserviceapp/ui/views/base/MVPView.java
@@ -0,0 +1,8 @@
+package org.mifos.selfserviceapp.ui.views.base;
+
+/**
+ * @author ishan
+ * @since 19/05/16
+ */
+public interface MVPView {
+}
