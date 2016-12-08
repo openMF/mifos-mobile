a76bdc1378b373fe91b2f6aec4fe71ad127a1161
Michael S <sosnickm@wharton.upenn.edu>
2016-12-07 23:45:56 -0500

    refactor: Show Client Charge

--- app/src/main/java/org/mifos/selfserviceapp/models/Charge.java
+++ app/src/main/java/org/mifos/selfserviceapp/models/Charge.java
@@ -8,12 +8,27 @@ import java.util.List;
  * @since 16/8/16.
  */
 public class Charge {
+
     private int id;
+    private int clientId;
     private int chargeId;
     private String name;
-    private double amount;
+
     private List<Long> dueDate = new ArrayList<>();
+    private ChargeTimeType chargeTimeType;
+    private ChargeCalculationType chargeCalculationType;
     private Currency currency;
+
+    private double amount = 0.0;
+    private double amountPaid = 0.0;
+    private double amountWaived = 0.0;
+    private double amountWrittenOff = 0.0;
+    private double amountOutstanding = 0.0;
+
+    private boolean penalty = false;
+    private boolean isActive = true;
+    private boolean isPaid = false;
+    private boolean isWaived = false;
 
     public int getId() {
         return id;
@@ -29,6 +44,14 @@ public class Charge {
 
     public void setChargeId(int chargeId) {
         this.chargeId = chargeId;
+    }
+
+    public int getClientId() {
+        return chargeId;
+    }
+
+    public void setClientId(int chargeId) {
+        this.clientId = clientId;
     }
 
     public String getName() {
@@ -47,6 +70,34 @@ public class Charge {
         this.amount = amount;
     }
 
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+
+    public void setAmountPaid(double amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    public double getAmountWaived() {
+        return amountWaived;
+    }
+
+    public void setAmountWaived(double amountWaived) {
+        this.amountWaived = amountWaived;
+    }
+
+    public double getAmountWrittenOff() {
+        return amountWrittenOff;
+    }
+
+    public void setAmountWrittenOff(double amountWrittenOff) { this.amountWrittenOff = amountWrittenOff; }
+
+    public double getAmountOutstanding() {
+        return amountOutstanding;
+    }
+
+    public void setAmountOutstanding(double amountOutstanding) { this.amountOutstanding = amountOutstanding; }
+
     public List<Long> getDueDate() {
         return dueDate;
     }
@@ -63,9 +114,58 @@ public class Charge {
         this.currency = currency;
     }
 
+    public ChargeTimeType getChargeTimeType() {
+        return chargeTimeType;
+    }
+
+    public void setChargeTimeType(ChargeTimeType chargeTimeType) { this.chargeTimeType = chargeTimeType; }
+
+    public ChargeCalculationType getChargeCalculationType() {
+        return chargeCalculationType;
+    }
+
+    public void setChargeCalculationType(ChargeCalculationType chargeCalculationType) { this.chargeCalculationType = chargeCalculationType; }
+
+    public boolean getPenalty() {
+        return penalty;
+    }
+
+    public void setPenalty(boolean penalty) {
+        this.penalty = penalty;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public boolean getIsPaid() {
+        return isPaid;
+    }
+
+    public void setIsPaid(boolean isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    public boolean getIsWaived() {
+        return isWaived;
+    }
+
+    public void setIsWaived(boolean isWaived) {
+        this.isWaived = isWaived;
+    }
+
+
     public static class Currency {
         private String code;
         private String name;
+        private int decimalPlaces;
+        private String displaySymbol;
+        private String nameCode;
+        private String displayLabel;
 
         public String getCode() {
             return code;
@@ -75,13 +175,95 @@ public class Charge {
             this.code = code;
         }
 
-        public String getName() {
-            return name;
+        public String getName() { return name; }
+
+        public void setName(String name) { this.name = name; }
+
+        public int getDecimalPlaces() {
+            return decimalPlaces;
         }
 
-        public void setName(String name) {
-            this.name = name;
+        public void setDecimalPlaces(int decimalPlaces) { this.decimalPlaces = decimalPlaces; }
+
+        public String getDisplaySymbol() {
+            return displaySymbol;
+        }
+
+        public void setDisplaySymbol(String displaySymbol) { this.displaySymbol = displaySymbol; }
+
+        public String getNameCode() {
+            return nameCode;
+        }
+
+        public void setNameCode(String NameCode) { this.nameCode = nameCode; }
+
+        public String getDisplayLabel() {
+            return displayLabel;
+        }
+
+        public void setDisplayLabel(String displayLabel) { this.displayLabel = displayLabel; }
+
+    }
+
+
+    public static class ChargeTimeType {
+        private int id;
+        private String code;
+        private String value;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
         }
     }
 
+
+    public static class ChargeCalculationType {
+        private int id;
+        private String code; // example "chargeCalculationType.flat"
+        private String value;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
 }
