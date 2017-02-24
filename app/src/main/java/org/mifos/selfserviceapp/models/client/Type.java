210181d0300727664a3f56e0fd885eb67fc8abff
Rajan Maurya <rajanmaurya154@gmail.com>
2017-02-24 16:03:46 +0530

    refactor: Recent Transaction with enhancement

---
+++ app/src/main/java/org/mifos/selfserviceapp/models/client/Type.java
@@ -0,0 +1,79 @@
+package org.mifos.selfserviceapp.models.client;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Rajan Maurya on 23/02/17.
+ */
+
+public class Type implements Parcelable {
+
+    @SerializedName("id")
+    Integer id;
+
+    @SerializedName("code")
+    String code;
+
+    @SerializedName("value")
+    String value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id);
+        dest.writeString(this.code);
+        dest.writeString(this.value);
+    }
+
+    public Type() {
+    }
+
+    protected Type(Parcel in) {
+        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.code = in.readString();
+        this.value = in.readString();
+    }
+
+    public static final Parcelable.Creator<Type> CREATOR = new Parcelable.Creator<Type>() {
+        @Override
+        public Type createFromParcel(Parcel source) {
+            return new Type(source);
+        }
+
+        @Override
+        public Type[] newArray(int size) {
+            return new Type[size];
+        }
+    };
+}
