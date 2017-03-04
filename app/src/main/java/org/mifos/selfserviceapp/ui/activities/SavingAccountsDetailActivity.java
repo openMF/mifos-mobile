5831805d93bab1535183b1a91f2edecb9da9aeb3
prashanthkomuravelli <prashanthkomuravelli@gmail.com>
2017-03-04 09:50:48 +0530

    Resolving Issue #82 i,e Savings Account Details

--- app/src/main/java/org/mifos/selfserviceapp/ui/activities/SavingAccountsDetailActivity.java
+++ app/src/main/java/org/mifos/selfserviceapp/ui/activities/SavingAccountsDetailActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.mifos.selfserviceapp.R;
 import org.mifos.selfserviceapp.models.accounts.savings.SavingAccount;
 import org.mifos.selfserviceapp.presenters.SavingAccountsDetailPresenter;
