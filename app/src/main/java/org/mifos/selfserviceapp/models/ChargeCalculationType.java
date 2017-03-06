d7e1eb291169ebf73393dff0242637e9f65dec7b
Rajan Maurya <rajanmaurya154@gmail.com>
2017-03-06 22:41:13 +0530

    feat: Loan Account Application

--- app/src/main/java/org/mifos/selfserviceapp/models/ChargeCalculationType.java
+++ app/src/main/java/org/mifos/selfserviceapp/models/ChargeCalculationType.java
@@ -1,10 +1,13 @@
 package org.mifos.selfserviceapp.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by michaelsosnick on 12/11/16.
  */
 
-public class ChargeCalculationType {
+public class ChargeCalculationType implements Parcelable {
     private int id;
     private String code; // example "chargeCalculationType.flat"
     private String value;
@@ -32,4 +35,38 @@ public class ChargeCalculationType {
     public void setValue(String value) {
         this.value = value;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.code);
+        dest.writeString(this.value);
+    }
+
+    public ChargeCalculationType() {
+    }
+
+    protected ChargeCalculationType(Parcel in) {
+        this.id = in.readInt();
+        this.code = in.readString();
+        this.value = in.readString();
+    }
+
+    public static final Parcelable.Creator<ChargeCalculationType> CREATOR =
+            new Parcelable.Creator<ChargeCalculationType>() {
+                @Override
+                public ChargeCalculationType createFromParcel(Parcel source) {
+                    return new ChargeCalculationType(source);
+                }
+
+                @Override
+                public ChargeCalculationType[] newArray(int size) {
+                    return new ChargeCalculationType[size];
+                }
+            };
 }
