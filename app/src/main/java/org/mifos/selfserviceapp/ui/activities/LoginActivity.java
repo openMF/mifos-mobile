5297f8e7439de5c01f1fa4436caee165580e3d65
prashanthkomuravelli <prashanthkomuravelli@gmail.com>
2017-03-29 11:00:01 +0530

    feat : auto completion feature of username in login screen

--- app/src/main/java/org/mifos/selfserviceapp/ui/activities/LoginActivity.java
+++ app/src/main/java/org/mifos/selfserviceapp/ui/activities/LoginActivity.java
@@ -1,13 +1,15 @@
 package org.mifos.selfserviceapp.ui.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
 import org.mifos.selfserviceapp.R;
 import org.mifos.selfserviceapp.presenters.LoginPresenter;
 import org.mifos.selfserviceapp.ui.activities.base.BaseActivity;
@@ -15,6 +17,9 @@ import org.mifos.selfserviceapp.ui.views.LoginView;
 import org.mifos.selfserviceapp.utils.Network;
 import org.mifos.selfserviceapp.utils.Constants;
 import org.mifos.selfserviceapp.utils.Toaster;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -36,7 +41,7 @@ public class LoginActivity extends BaseActivity implements LoginView {
     AppCompatButton btnLogin;
 
     @BindView(R.id.et_username)
-    EditText etUsername;
+    AutoCompleteTextView etUsername;
 
     @BindView(R.id.et_password)
     EditText etPassword;
@@ -45,7 +50,10 @@ public class LoginActivity extends BaseActivity implements LoginView {
     LinearLayout llLogin;
 
     private boolean loginStatus;
-
+    public static final String PREFS_NAME = "PingBusPrefs";
+    public static final String PREFS_SEARCH_HISTORY = "SearchHistory";
+    private SharedPreferences settings;
+    private Set<String> usernames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +62,9 @@ public class LoginActivity extends BaseActivity implements LoginView {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         loginPresenter.attachView(this);
+        settings = getSharedPreferences(PREFS_NAME, 0);
+        usernames = settings.getStringSet(PREFS_SEARCH_HISTORY, new HashSet<String>());
+        setAutoCompleteSource(etUsername.toString());
     }
 
     @Override
@@ -62,8 +73,24 @@ public class LoginActivity extends BaseActivity implements LoginView {
         showToast(getString(R.string.toast_welcome, userName));
         llLogin.setVisibility(View.GONE);
         loginPresenter.loadClient();
-    }
+        if (!usernames.contains(userName)) {
+            usernames.add(userName);
+            setAutoCompleteSource(userName);
 
+        }
+    }
+    private void saveprfs(String userName) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putStringSet(PREFS_SEARCH_HISTORY , usernames);
+        editor.commit();
+    }
+    private void setAutoCompleteSource(String userName) {
+        int size = usernames.size();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this ,
+                android.R.layout.simple_list_item_1 , usernames.toArray(new String[size]));
+        etUsername.setAdapter(adapter);
+    }
     @Override
     public void showProgress() {
         if (!loginStatus) {
@@ -108,5 +135,11 @@ public class LoginActivity extends BaseActivity implements LoginView {
     protected void onDestroy() {
         super.onDestroy();
         loginPresenter.detachView();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveprfs(etUsername.toString());
     }
 }
