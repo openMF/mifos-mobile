f028e79b07f45965ec67b4acc4cbe1913cb0786d
naman14 <namandwivedi14@gmail.com>
2017-04-06 23:52:55 +0530

    remove unused import, fix pmd check

--- app/src/main/java/org/mifos/selfserviceapp/ui/fragments/SavingAccountsTransactionFragment.java
+++ app/src/main/java/org/mifos/selfserviceapp/ui/fragments/SavingAccountsTransactionFragment.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -122,6 +121,7 @@ public class SavingAccountsTransactionFragment extends BaseFragment
         MenuItem item = menu.findItem(R.id.item_transactions);
         item.setVisible(false);
     }
+
     @Override
     public void showUserInterface() {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
