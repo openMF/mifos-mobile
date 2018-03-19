5f748948c8eaf48d84ba1a13eeac22e539a1235b
miPlodder <academic.rahul@gmail.com>
2018-03-19 21:48:59 +0530

    Fix #747 : Added Checks for empty Edittexts and for validity of Url in Update Endpoints Setting

--- app/src/main/java/org/mifos/mobilebanking/utils/ConfigurationDialogFragmentCompat.java
+++ app/src/main/java/org/mifos/mobilebanking/utils/ConfigurationDialogFragmentCompat.java
@@ -2,9 +2,12 @@ package org.mifos.mobilebanking.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.preference.DialogPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceDialogFragmentCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -17,18 +20,27 @@ import org.mifos.mobilebanking.ui.activities.LoginActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by dilpreet on 11/03/18.
  */
 
 public class ConfigurationDialogFragmentCompat extends PreferenceDialogFragmentCompat implements
-        DialogPreference.TargetFragment {
+        DialogPreference.TargetFragment, TextWatcher {
 
     @BindView(R.id.et_tenant)
     EditText etTenant;
 
     @BindView(R.id.et_base_url)
     EditText etBaseUrl;
+
+    @BindView(R.id.til_tenant)
+    TextInputLayout tilTenant;
+
+    @BindView(R.id.til_base_url)
+    TextInputLayout tilBaseUrl;
 
     private PreferencesHelper preferencesHelper;
 
@@ -47,13 +59,15 @@ public class ConfigurationDialogFragmentCompat extends PreferenceDialogFragmentC
 
         etBaseUrl.setText(preference.getBaseUrl());
         etTenant.setText(preference.getTenant());
-
+        
         etBaseUrl.setSelection(etBaseUrl.getText().length());
+        etTenant.addTextChangedListener(this);
+        etBaseUrl.addTextChangedListener(this);
     }
 
     @Override
     public void onDialogClosed(boolean positiveResult) {
-        if (positiveResult) {
+        if (positiveResult && !isFieldEmpty() && isUrlValid()) {
             ConfigurationPreference preference = (ConfigurationPreference) getPreference();
             preference.updateConfigurations(etBaseUrl.getText().toString(), etTenant.getText().
                     toString());
@@ -73,5 +87,56 @@ public class ConfigurationDialogFragmentCompat extends PreferenceDialogFragmentC
     @Override
     public Preference findPreference(CharSequence key) {
         return getPreference();
+    }
+
+    public boolean isFieldEmpty() {
+        if (etBaseUrl.getText().toString().trim().length() == 0) {
+            return true;
+        }
+        if (etTenant.getText().toString().trim().length() == 0) {
+            return  true;
+        }
+        return  false;
+    }
+
+    public boolean isUrlValid() {
+        try {
+            new URL(etBaseUrl.getText().toString());
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
+        }
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (s.toString().length() == 0) {
+            if (etBaseUrl.getText().toString().length() == 0) {
+                tilBaseUrl.setErrorEnabled(true);
+                tilBaseUrl.setError(getString(R.string.error_validation_blank,
+                        getString(R.string.base_url)));
+            }
+            if (etTenant.getText().toString().length() == 0) {
+                tilTenant.setErrorEnabled(true);
+                tilTenant.setError(getString(R.string.error_validation_blank,
+                        getString(R.string.tenant)));
+            }
+        } else {
+            if (etBaseUrl.getText().toString().length() != 0) {
+                tilBaseUrl.setErrorEnabled(false);
+            }
+            if (etTenant.getText().toString().length() != 0) {
+                tilTenant.setErrorEnabled(false);
+            }
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
     }
 }
