dcf9dd1867c6c0f3fddbaeedf77e5fbe1f46f9dc
ivary43 <contactmanish16@gmail.com>
2018-05-20 02:32:00 +0530

    feat:added Qr import functionality to add beneficiary

--- app/src/main/java/org/mifos/mobilebanking/ui/fragments/BeneficiaryAddOptionsFragment.java
+++ app/src/main/java/org/mifos/mobilebanking/ui/fragments/BeneficiaryAddOptionsFragment.java
@@ -1,6 +1,7 @@
 package org.mifos.mobilebanking.ui.fragments;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,13 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import org.mifos.mobilebanking.R;
 import org.mifos.mobilebanking.ui.activities.base.BaseActivity;
 import org.mifos.mobilebanking.ui.enums.BeneficiaryState;
+import org.mifos.mobilebanking.ui.enums.RequestAccessType;
 import org.mifos.mobilebanking.ui.fragments.base.BaseFragment;
 import org.mifos.mobilebanking.utils.CheckSelfPermissionAndRequest;
 import org.mifos.mobilebanking.utils.Constants;
 import org.mifos.mobilebanking.utils.Toaster;
+
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -27,6 +31,8 @@ import butterknife.OnClick;
 public class BeneficiaryAddOptionsFragment extends BaseFragment {
 
     private View rootView;
+    private boolean external_storage_read_status = false;
+    private boolean external_storage_write_status = false;
 
     public static BeneficiaryAddOptionsFragment newInstance() {
         BeneficiaryAddOptionsFragment fragment = new BeneficiaryAddOptionsFragment();
@@ -68,29 +74,108 @@ public class BeneficiaryAddOptionsFragment extends BaseFragment {
             ((BaseActivity) getActivity()).replaceFragment(QrCodeReaderFragment.
                     newInstance(), true, R.id.container);
         } else {
-            requestPermission();
+            requestPermission(RequestAccessType.CAMERA);
+        }
+    }
+
+    /**
+     * It first checks Storage Read and Write Permission then if both of them are true then it opens
+     * Intent to all gallery app
+     */
+    @OnClick(R.id.ll_upload_beneficiary_qrcode)
+    public void addByImportingQrCode() {
+
+        //request permission for writing external storage
+        accessReadWriteAccess();
+        if (external_storage_write_status && external_storage_read_status) {
+            Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            getIntent.setType("image/*");
+            Intent pickIntent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            pickIntent.setType("image/*");
+            Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+            startActivityForResult(chooserIntent, Constants.GALLERY_QR_PICK);
+        }
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Constants.GALLERY_QR_PICK && data != null) {
+            getActivity().getSupportFragmentManager().popBackStack();
+            ((BaseActivity) getActivity()).replaceFragment(QrCodeImportFragment
+                    .newInstance(data.getData()), true, R.id.container);
+        }
+    }
+
+    public void accessReadWriteAccess() {
+        if (CheckSelfPermissionAndRequest.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            external_storage_read_status = true;
+        } else {
+            requestPermission(RequestAccessType.EXTERNAL_STORAGE_READ);
+        }
+        if (CheckSelfPermissionAndRequest.checkSelfPermission(getActivity(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            external_storage_write_status = true;
+        } else {
+            requestPermission(RequestAccessType.EXTERNAL_STORAGE_WRITE);
         }
     }
 
     /**
      * Uses {@link CheckSelfPermissionAndRequest} to check for runtime permissions
      */
-    private void requestPermission() {
-        CheckSelfPermissionAndRequest.requestPermission(
-                (BaseActivity) getActivity(),
-                Manifest.permission.CAMERA,
-                Constants.PERMISSIONS_REQUEST_CAMERA,
-                getResources().getString(
-                        R.string.dialog_message_camera_permission_denied_prompt),
-                getResources().getString(R.string.dialog_message_camera_permission_never_ask_again),
-                Constants.PERMISSIONS_CAMERA_STATUS);
+    private void requestPermission(RequestAccessType requestAccessType) {
+
+        switch (requestAccessType) {
+            case CAMERA: {
+                CheckSelfPermissionAndRequest.requestPermission(
+                        (BaseActivity) getActivity(),
+                        Manifest.permission.CAMERA,
+                        Constants.PERMISSIONS_REQUEST_CAMERA,
+                        getResources().getString(
+                                R.string.dialog_message_camera_permission_denied_prompt),
+                        getResources().getString(R
+                                .string.dialog_message_camera_permission_never_ask_again),
+                        Constants.PERMISSIONS_CAMERA_STATUS);
+            }
+            break;
+            case EXTERNAL_STORAGE_READ: {
+                CheckSelfPermissionAndRequest.requestPermission((BaseActivity) getActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Constants.PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE,
+                        getResources().getString(R
+                                .string.dialog_message_storage_permission_denied_prompt),
+                        getResources()
+                                .getString(R.string
+                                        .dialog_message_read_storage_permission_never_ask_again),
+                        Constants.PERMISSIONS_STORAGE_STATUS);
+
+            }
+            break;
+            case EXTERNAL_STORAGE_WRITE: {
+                CheckSelfPermissionAndRequest.requestPermission((BaseActivity) getActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Constants.PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE,
+                        getResources().getString(R
+                                .string.dialog_message_storage_permission_denied_prompt),
+                        getResources().getString(R
+                                .string.dialog_message_write_storage_permission_never_ask_again),
+                        Constants.PERMISSIONS_STORAGE_STATUS);
+            }
+
+        }
+
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         switch (requestCode) {
-            case Constants.PERMISSIONS_REQUEST_CAMERA : {
+            case Constants.PERMISSIONS_REQUEST_CAMERA: {
 
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -103,6 +188,29 @@ public class BeneficiaryAddOptionsFragment extends BaseFragment {
                             .getString(R.string.permission_denied_camera));
                 }
             }
+            break;
+            case Constants.PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    external_storage_read_status = true;
+                } else {
+                    Toaster.show(rootView, getResources()
+                            .getString(R.string.permission_denied_storage));
+                }
+
+            }
+            break;
+            case Constants.PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    external_storage_write_status = true;
+                } else {
+                    Toaster.show(rootView, getResources()
+                            .getString(R.string.permission_denied_storage));
+                }
+            }
+
         }
     }
 }
