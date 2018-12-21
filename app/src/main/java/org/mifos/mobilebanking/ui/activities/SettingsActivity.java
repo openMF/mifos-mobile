881385606a2cf43af7024fb04ec4a2c1dc4cc351
Saksham Handu <saksham.handu2015@vit.ac.in>
2018-12-21 21:26:36 +0530

    Fix #969: Language changes on Home Activity (#970)

--- app/src/main/java/org/mifos/mobilebanking/ui/activities/SettingsActivity.java
+++ app/src/main/java/org/mifos/mobilebanking/ui/activities/SettingsActivity.java
@@ -1,5 +1,6 @@
 package org.mifos.mobilebanking.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.mifos.mobilebanking.R;
@@ -15,5 +16,12 @@ public class SettingsActivity extends BaseActivity {
         setToolbarTitle(getString(R.string.settings));
         showBackButton();
         replaceFragment(SettingsFragment.newInstance(), false, R.id.container);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
     }
 }
