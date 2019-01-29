05b697087de2e928fca8886c46493dad91d5cfda
Abhinav <abhin2149@gmail.com>
2019-01-29 17:15:40 +0530

    fix: Date picker displays the previous date as selected by the user
    issue:#983

--- app/src/main/java/org/mifos/mobilebanking/utils/MFDatePicker.java
+++ app/src/main/java/org/mifos/mobilebanking/utils/MFDatePicker.java
@@ -14,9 +14,9 @@ import android.text.format.DateFormat;
 import android.widget.DatePicker;
 
 import org.mifos.mobilebanking.R;
-
 import java.util.Calendar;
 import java.util.Date;
+
 
 /**
  * Created by ishankhanna on 30/06/14.
@@ -28,6 +28,7 @@ public class MFDatePicker extends DialogFragment implements DatePickerDialog.OnD
     static Calendar calendar;
     private String startDateString;
     private long startDate;
+    static Calendar selectedDate;
     private int datePickerType = ALL_DAYS;
 
     /* Constants used to select which type of date picker is being called */
@@ -40,6 +41,7 @@ public class MFDatePicker extends DialogFragment implements DatePickerDialog.OnD
     static {
 
         calendar = Calendar.getInstance();
+        selectedDate=calendar;
         dateSet = new StringBuilder()
                 .append(calendar.get(Calendar.DAY_OF_MONTH) < 10 ?
                         ("0" + calendar.get(Calendar.DAY_OF_MONTH))
@@ -67,6 +69,7 @@ public class MFDatePicker extends DialogFragment implements DatePickerDialog.OnD
 
         mfDatePicker.setArguments(args);
 
+
         mfDatePicker.onDatePickListener = (OnDatePickListener) fragment;
         return mfDatePicker;
     }
@@ -78,12 +81,17 @@ public class MFDatePicker extends DialogFragment implements DatePickerDialog.OnD
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        DatePickerDialog dialog = new DatePickerDialog(getActivity(),
+        int year=selectedDate.get(Calendar.YEAR);
+        int month=selectedDate.get(Calendar.MONTH);
+        int day=selectedDate.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(),
                 R.style.MaterialDatePickerTheme,
                 this,
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH));
+               year,
+                month,
+                day);
 
         Bundle args = getArguments();
         this.datePickerType = args.getInt(Constants.DATE_PICKER_TYPE, this.ALL_DAYS);
@@ -97,6 +105,7 @@ public class MFDatePicker extends DialogFragment implements DatePickerDialog.OnD
                 dialog.getDatePicker().setMaxDate(new Date().getTime());
                 break;
         }
+
         return dialog;
     }
 
@@ -106,6 +115,7 @@ public class MFDatePicker extends DialogFragment implements DatePickerDialog.OnD
         Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, day);
         Date date = calendar.getTime();
+        selectedDate=calendar;
         onDatePickListener.onDatePicked(startDateString = DateFormat.
                 format("dd-MM-yyyy", date).toString());
         startDate = DateHelper.getDateAsLongFromString(startDateString, "dd-MM-yyyy");
