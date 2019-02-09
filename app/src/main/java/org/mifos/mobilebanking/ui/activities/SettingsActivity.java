ebb4a0339760efcb68b895eaa209bae45bb052ef
Rahul Jha <jharahul1998@gmail.com>
2019-02-09 17:28:24 +0530

    fix #1009: New instance of home created only if language is changed (#1016)

--- app/src/main/java/org/mifos/mobilebanking/ui/activities/SettingsActivity.java
+++ app/src/main/java/org/mifos/mobilebanking/ui/activities/SettingsActivity.java
@@ -2,12 +2,16 @@ package org.mifos.mobilebanking.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 
 import org.mifos.mobilebanking.R;
 import org.mifos.mobilebanking.ui.activities.base.BaseActivity;
 import org.mifos.mobilebanking.ui.fragments.SettingsFragment;
+import org.mifos.mobilebanking.utils.Constants;
 
 public class SettingsActivity extends BaseActivity {
+
+    private boolean hasSettingsChanged;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,19 @@ public class SettingsActivity extends BaseActivity {
         setToolbarTitle(getString(R.string.settings));
         showBackButton();
         replaceFragment(SettingsFragment.newInstance(), false, R.id.container);
+        if (getIntent().hasExtra(Constants.HAS_SETTINGS_CHANGED)) {
+            hasSettingsChanged = getIntent().getBooleanExtra(Constants.HAS_SETTINGS_CHANGED,
+                    false);
+        }
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Intent i = new Intent(this, HomeActivity.class);
-        startActivity(i);
+        if (hasSettingsChanged) {
+            ActivityCompat.finishAffinity(this);
+            Intent i = new Intent(this, HomeActivity.class);
+            startActivity(i);
+        }
     }
 }
