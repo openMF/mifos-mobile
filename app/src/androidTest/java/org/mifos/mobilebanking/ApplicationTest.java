077840a7f4ae082ffb7d26f94bb3cf457dd4de5f
therajanmaurya <rajanmaurya154@gmail.com>
2019-03-22 18:12:10 +0530

    Fix: Intrumentation test setup according to new API

--- app/src/androidTest/java/org/mifos/mobilebanking/ApplicationTest.java
+++ app/src/androidTest/java/org/mifos/mobilebanking/ApplicationTest.java
@@ -1,13 +1,24 @@
 package org.mifos.mobilebanking;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("org.mifos.mobilebanking", appContext.getPackageName());
     }
 }
