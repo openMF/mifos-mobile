99e582477aa98d1158ddc0b9aa1918e32e398c2e
Prashant Khandelwal <f2016930@pilani.bits-pilani.ac.in>
2019-06-02 12:29:52 +0530

    Fix #808 : Merge fix of PR 812

--- app/src/main/java/org/mifos/mobile/ui/fragments/SavingsMakeTransferFragment.java
+++ app/src/main/java/org/mifos/mobile/ui/fragments/SavingsMakeTransferFragment.java
@@ -123,6 +123,8 @@ public class SavingsMakeTransferFragment extends BaseFragment implements
     private AccountOptionsTemplate accountOptionsTemplate;
     private String transferType, payTo, payFrom;
     private long accountId;
+    private double outStandingBalance;
+    private boolean isLoanRepayment;
     private SweetUIErrorHandler sweetUIErrorHandler;
 
     /**
@@ -144,12 +146,29 @@ public class SavingsMakeTransferFragment extends BaseFragment implements
         return transferFragment;
     }
 
+    public static SavingsMakeTransferFragment newInstance(long accountId, double outstandingBalance,
+                                                          String transferType) {
+        SavingsMakeTransferFragment transferFragment = new SavingsMakeTransferFragment();
+        Bundle args = new Bundle();
+        args.putLong(Constants.ACCOUNT_ID, accountId);
+        args.putString(Constants.TRANSFER_TYPE, transferType);
+        args.putDouble(Constants.OUTSTANDING_BALANCE, outstandingBalance);
+        args.putBoolean(Constants.LOAN_REPAYMENT, true);
+        transferFragment.setArguments(args);
+        return transferFragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             accountId = getArguments().getLong(Constants.ACCOUNT_ID);
             transferType = getArguments().getString(Constants.TRANSFER_TYPE);
+
+            if (getArguments().getBoolean(Constants.LOAN_REPAYMENT, false)) {
+                isLoanRepayment = true;
+                outStandingBalance = getArguments().getDouble(Constants.OUTSTANDING_BALANCE);
+            }
         }
         setHasOptionsMenu(true);
     }
@@ -251,6 +270,12 @@ public class SavingsMakeTransferFragment extends BaseFragment implements
         spPayTo.setOnItemSelectedListener(this);
         transferDate = DateHelper.getSpecificFormat(DateHelper.FORMAT_dd_MMMM_yyyy,
                 MFDatePicker.getDatePickedAsString());
+
+        if (isLoanRepayment) {
+            etAmount.setText(String.valueOf(outStandingBalance));
+            etAmount.setFocusable(false);
+        }
+
     }
 
     /**
