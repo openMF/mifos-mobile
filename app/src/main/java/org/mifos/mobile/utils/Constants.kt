ddd6ceadf184aa33e872ecc33a507960470eaee9
Ashwin R <ashwinkey04@gmail.com>
2020-06-29 23:45:03 +0530

    Convert Passcode activity and constants to Kotlin

---
+++ app/src/main/java/org/mifos/mobile/utils/Constants.kt
@@ -0,0 +1,68 @@
+package org.mifos.mobile.utils
+
+/**
+ * @author Vishwajeet
+ * @since 21/06/16.
+ */
+object Constants {
+    const val CLIENT_NAME = "clientName"
+    const val CLIENT_ID = "clientId"
+    const val ACCOUNT_ID = "accountId"
+    const val LOAN_ID = "loanId"
+    const val SAVINGS_ID = "savingsId"
+    const val BASIC = "Basic "
+    const val BACK_SLASH = "/"
+    const val SAVINGS_ACCOUNTS = "savingsAccounts"
+    const val LOAN_ACCOUNTS = "loanAccounts"
+    const val SHARE_ACCOUNTS = "shareAccounts"
+    const val RECURRING_ACCOUNTS = "recurringdepositaccounts"
+    const val ACCOUNT_TYPE = "accountType"
+    const val REPAYMENT_SCHEDULE = "repaymentSchedule"
+    const val TRANSACTIONS = "transactions"
+    const val DFRAG_DATE_PICKER = "Date Picker"
+    const val TRANSFER_TYPE = "transfer_type"
+    const val TRANSFER_QUICK = "transfer_quick"
+    const val TRANSFER_PAY_TO = "transfer_pay_to"
+    const val TRANSFER_PAY_FROM = "transfer_pay_from"
+    const val ACCOUNT_POSITION = "account_position"
+    const val BENEFICIARY = "beneficiary"
+    const val BENEFICIARY_STATE = "beneficiary_state"
+    const val LOAN_STATE = "loanType"
+    const val LOAN_ACCOUNT = "loanAccount"
+    const val PAYLOAD = "payload"
+    const val REQUEST_PERMISSION_SETTING = 254
+    const val PERMISSIONS_REQUEST_CAMERA = 1
+    const val PERMISSIONS_REQUEST_READ_PHONE_STATE = 2
+    const val PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 3
+    const val PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 4
+    const val PERMISSIONS_CAMERA_STATUS = "camera_status"
+    const val PERMISSIONS_STORAGE_STATUS = "storage_status"
+    const val PERMISSIONS_READ_PHONE_STATE_STATUS = "read_phone_status"
+    const val INTIAL_LOGIN = "initial_login"
+    const val CHARGE_TYPE = "charge_type"
+    const val QR_DATA = "qrcode_data"
+    const val TEMPLATE = "template"
+    const val RECENT_TRANSACTIONS = "recent_transactions"
+    const val CHARGES = "charges"
+    const val HELP = "help"
+    const val USER_PROFILE = "user_profile"
+    const val USER_DETAILS = "user_details"
+    const val TOTAL_LOAN = "total_loan"
+    const val TOTAL_SAVINGS = "total_savings"
+    const val TOKEN = "token"
+    const val REGISTER_ON_SERVER = "RegisterOnServer"
+    const val NOTIFY_HOME_FRAGMENT = "NotifyHomeFragment"
+    const val QR_IMAGE_URI = "qr_image_uri"
+    const val GALLERY_QR_PICK = 1
+    const val FRAME_RECT = "FrameRect"
+    const val SOURCE_URI = "SourceUri"
+    const val GUARANTOR_DETAILS = "guarantorDetails"
+    const val GUARANTOR_STATE = "guarantorState"
+    const val INDEX = "index"
+    const val DATE_PICKER_TYPE = "datePickerType"
+    const val HAS_SETTINGS_CHANGED = "hasSettingsChanged"
+    const val PASSWORD = "Password"
+    const val SAVINGS_ACCOUNT_STATE = "savingsaccountstate"
+    const val OUTSTANDING_BALANCE = "outstanding_balance"
+    const val LOAN_REPAYMENT = "loan_repayment"
+}
