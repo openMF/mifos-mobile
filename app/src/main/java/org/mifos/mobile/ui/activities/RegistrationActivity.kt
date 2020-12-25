663890accbc5c0e67ca9824f89a616bf0768fb65
PatelVatsalB21 <vatsal9974702102@gmail.com>
2020-12-25 23:26:37 +0530

    fix_#1492: No warning message when going back from register account fixed

--- app/src/main/java/org/mifos/mobile/ui/activities/RegistrationActivity.kt
+++ app/src/main/java/org/mifos/mobile/ui/activities/RegistrationActivity.kt
@@ -1,10 +1,12 @@
 package org.mifos.mobile.ui.activities
 
+import android.content.DialogInterface
 import android.os.Bundle
 
 import org.mifos.mobile.R
 import org.mifos.mobile.ui.activities.base.BaseActivity
 import org.mifos.mobile.ui.fragments.RegistrationFragment
+import org.mifos.mobile.utils.MaterialDialog
 
 class RegistrationActivity : BaseActivity() {
 
@@ -12,5 +14,17 @@ class RegistrationActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         replaceFragment(RegistrationFragment.newInstance(), false, R.id.container)
+    }
+
+    override fun onBackPressed() {
+        MaterialDialog.Builder().init(this)
+                .setTitle(getString(R.string.dialog_cancel_registration_title))
+                .setMessage(getString(R.string.dialog_cancel_registration_message))
+                .setPositiveButton(getString(R.string.cancel),
+                        DialogInterface.OnClickListener { _, _ -> super.onBackPressed() })
+                .setNegativeButton(R.string.continue_str,
+                        DialogInterface.OnClickListener { dialog, _ -> dialog.dismiss() })
+                .createMaterialDialog()
+                .show()
     }
 }
