6e02c928dfdd025783b659b7f883b2cf80c25537
Rahul Gill <76812499+rahul-gill@users.noreply.github.com>
2022-04-15 00:02:31 +0530

    Fixes wrong parcelable impl. in Client model (#1838)

--- app/src/main/java/org/mifos/mobile/models/client/ClientClassification.kt
+++ app/src/main/java/org/mifos/mobile/models/client/ClientClassification.kt
@@ -1,12 +1,15 @@
 package org.mifos.mobile.models.client
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * Created by dilpreet on 10/7/17.
  */
-
+@Parcelize
 data class ClientClassification(
         var id: Int,
         var name: String? = null,
         var active: Boolean,
         var mandatory: Boolean
-)
+) : Parcelable
