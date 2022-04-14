b229e2f66ca5eaa8067d7508dbb0d4c042661f10
Rahul Gill <76812499+rahul-gill@users.noreply.github.com>
2022-04-15 00:02:31 +0530

    Fixes wrong parcelable impl. in Client model (#1838)

--- app/src/main/java/org/mifos/mobile/models/client/ClientType.kt
+++ app/src/main/java/org/mifos/mobile/models/client/ClientType.kt
@@ -1,12 +1,16 @@
 package org.mifos.mobile.models.client
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * Created by dilpreet on 10/7/17.
  */
 
+@Parcelize
 data class ClientType(
         var id: Int,
         var name: String? = null,
         var active: Boolean,
         var mandatory: Boolean
-)
+): Parcelable
--- app/src/main/java/org/mifos/mobile/models/client/Group.kt
+++ app/src/main/java/org/mifos/mobile/models/client/Group.kt
@@ -1,11 +1,14 @@
 package org.mifos.mobile.models.client
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * Created by dilpreet on 10/7/17.
  */
-
+@Parcelize
 data class Group(
         var id: Int,
         var accountNo: String? = null,
         var name: String? = null
-)
+): Parcelable
