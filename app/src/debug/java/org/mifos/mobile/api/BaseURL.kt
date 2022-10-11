f292b28118f0e6cb0eca8d71e3bcef4c8fd1e7e5
Rahul Gill <76812499+rahul-gill@users.noreply.github.com>
2022-10-11 16:34:37 +0530

    Update BaseURL.kt

--- app/src/debug/java/org/mifos/mobile/api/BaseURL.kt
+++ app/src/debug/java/org/mifos/mobile/api/BaseURL.kt
@@ -17,7 +17,7 @@ class BaseURL {
 
     companion object {
         const val API_ENDPOINT = "mobile.mifos.io"
-        const val API_PATH = "/fineract-provider/api/v1/self/"
+        const val API_PATH = "/fineract-provider/api/v1/"
         const val PROTOCOL_HTTPS = "https://"
     }
 }
