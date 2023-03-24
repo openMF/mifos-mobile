c27bb1883dc311a4566bfb15717eb5099fe83a9f
Pratyush Singh <aries.pratyush@gmail.com>
2023-03-25 02:15:23 +0530

    fix #1504:application allows passcode entered after three times

--- app/src/main/java/org/mifos/mobile/ui/activities/PassCodeActivity.kt
+++ app/src/main/java/org/mifos/mobile/ui/activities/PassCodeActivity.kt
@@ -5,10 +5,12 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-
+import android.widget.Toast
 import com.mifos.mobile.passcode.MifosPassCodeActivity
+import com.mifos.mobile.passcode.MifosPassCodeView
 import com.mifos.mobile.passcode.utils.EncryptionUtil
-
+import com.mifos.mobile.passcode.utils.EncryptionUtil.TYPE
+import com.mifos.mobile.passcode.utils.PasscodePreferencesHelper
 import org.mifos.mobile.R
 import org.mifos.mobile.utils.CheckSelfPermissionAndRequest
 import org.mifos.mobile.utils.Constants
@@ -16,18 +18,28 @@ import org.mifos.mobile.utils.MaterialDialog
 import org.mifos.mobile.utils.Toaster
 
 class PassCodeActivity : MifosPassCodeActivity() {
+
+    lateinit var passCodeHelper: PasscodePreferencesHelper
+    lateinit var mifosPassCodeView:MifosPassCodeView
+    private var counter=0
     private var currPassCode: String? = null
     private var isToUpdatePassCode: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        passCodeHelper = PasscodePreferencesHelper(this)
         if (!CheckSelfPermissionAndRequest.checkSelfPermission(this,
-                        Manifest.permission.READ_PHONE_STATE)) {
+                Manifest.permission.READ_PHONE_STATE)) {
             requestPermission()
         }
+
+        mifosPassCodeView = findViewById(com.mifos.mobile.passcode.R.id.pv_passcode)
+
         intent?.let {
             currPassCode = it.getStringExtra(Constants.CURR_PASSWORD)
             isToUpdatePassCode = it.getBooleanExtra(Constants.IS_TO_UPDATE_PASS_CODE, false)
         }
+
     }
 
     /**
@@ -35,13 +47,13 @@ class PassCodeActivity : MifosPassCodeActivity() {
      */
     private fun requestPermission() {
         CheckSelfPermissionAndRequest.requestPermission(
-                this,
-                Manifest.permission.READ_PHONE_STATE,
-                Constants.PERMISSIONS_REQUEST_READ_PHONE_STATE,
-                resources.getString(
-                        R.string.dialog_message_phone_state_permission_denied_prompt),
-                resources.getString(R.string.dialog_message_phone_state_permission_never_ask_again),
-                Constants.PERMISSIONS_READ_PHONE_STATE_STATUS)
+            this,
+            Manifest.permission.READ_PHONE_STATE,
+            Constants.PERMISSIONS_REQUEST_READ_PHONE_STATE,
+            resources.getString(
+                R.string.dialog_message_phone_state_permission_denied_prompt),
+            resources.getString(R.string.dialog_message_phone_state_permission_never_ask_again),
+            Constants.PERMISSIONS_READ_PHONE_STATE_STATUS)
     }
 
     override fun getLogo(): Int {
@@ -54,20 +66,20 @@ class PassCodeActivity : MifosPassCodeActivity() {
 
     override fun startLoginActivity() {
         MaterialDialog.Builder().init(this@PassCodeActivity)
-                .setCancelable(false)
-                .setMessage(R.string.login_using_password_confirmation)
-                .setPositiveButton(getString(R.string.logout),
-                        DialogInterface.OnClickListener { _, _ ->
-                            val i = Intent(this@PassCodeActivity,
-                                    LoginActivity::class.java)
-                            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            startActivity(i)
-                            finish()
-                        })
-                .setNegativeButton(getString(R.string.cancel),
-                        DialogInterface.OnClickListener { dialog, _ -> dialog.dismiss() })
-                .createMaterialDialog()
-                .show()
+            .setCancelable(false)
+            .setMessage(R.string.login_using_password_confirmation)
+            .setPositiveButton(getString(R.string.logout),
+                DialogInterface.OnClickListener { _, _ ->
+                    val i = Intent(this@PassCodeActivity,
+                        LoginActivity::class.java)
+                    i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(i)
+                    finish()
+                })
+            .setNegativeButton(getString(R.string.cancel),
+                DialogInterface.OnClickListener { dialog, _ -> dialog.dismiss() })
+            .createMaterialDialog()
+            .show()
     }
 
     override fun showToaster(view: View, msg: Int) {
@@ -76,5 +88,30 @@ class PassCodeActivity : MifosPassCodeActivity() {
 
     override fun getEncryptionType(): Int {
         return EncryptionUtil.MOBILE_BANKING
+    }
+
+    override fun passCodeEntered(passcode: String?) {
+            val passwordEntered = EncryptionUtil.getMobileBankingHash(mifosPassCodeView.passcode)
+            if (passCodeHelper.passCode.equals(passwordEntered)) {
+                counter=0
+                startNextActivity()
+            } else {
+                counter++
+                mifosPassCodeView.clearPasscodeField()
+                showToaster(findViewById(com.mifos.mobile.passcode.R.id.cl_rootview), R.string.incorrect_passcode)
+            }
+        if (counter > 3) {
+            Toast.makeText(
+                applicationContext, R.string.incorrect_passcode_more_than_three,
+                Toast.LENGTH_SHORT
+            ).show()
+            mifosPassCodeView.clearPasscodeField()
+            startLoginActivity()
+        }
+    }
+
+    override fun onResume() {
+        mifosPassCodeView.clearPasscodeField()
+        super.onResume()
     }
 }
