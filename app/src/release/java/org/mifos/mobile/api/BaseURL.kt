d9facf451bb9fb100f5dfd1a819a160e6779303a
Pratyush Singh <aries.pratyush@gmail.com>
2023-04-15 22:33:06 +0530

    fix #2110: modified the endpoint

--- app/src/release/java/org/mifos/mobile/api/BaseURL.kt
+++ app/src/release/java/org/mifos/mobile/api/BaseURL.kt
@@ -18,7 +18,7 @@ class BaseURL {
     }
 
     companion object {
-        const val API_ENDPOINT = "demo.mifos.community"
+        const val API_ENDPOINT = "demo.mifos.io"
         const val API_PATH = "/fineract-provider/api/v1/"
         const val PROTOCOL_HTTPS = "https://"
     }
