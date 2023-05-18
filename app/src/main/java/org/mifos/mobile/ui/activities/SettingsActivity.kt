54aad5b9bf8439e015b2768cf3a5ec996008546d
gururani-abhishek <gururani.abhishek@gmail.com>
2023-05-19 00:27:12 +0530

    migrating lower navigation drawer features from butterknife to viewbinding
    activities(4)
    	AboutUs, Help, PrivacyPolicy, Settings
    fragments(2)
    	AboutUsFragment, HelpFragment
    
    Overridden setContentVew(view : View?) {...} in BaseActvity to support calls for setContentView(view) inside activities.

--- app/src/main/java/org/mifos/mobile/ui/activities/SettingsActivity.kt
+++ app/src/main/java/org/mifos/mobile/ui/activities/SettingsActivity.kt
@@ -6,16 +6,19 @@ import android.os.Bundle
 import androidx.core.app.ActivityCompat
 
 import org.mifos.mobile.R
+import org.mifos.mobile.databinding.ActivitySettingsBinding
 import org.mifos.mobile.ui.activities.base.BaseActivity
 import org.mifos.mobile.ui.fragments.SettingsFragment
 import org.mifos.mobile.utils.Constants
 
 class SettingsActivity : BaseActivity() {
-
+    private lateinit var binding : ActivitySettingsBinding
     private var hasSettingsChanged = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setToolbarTitle(getString(R.string.settings))
         showBackButton()
         replaceFragment(SettingsFragment.newInstance(), false, R.id.container)
