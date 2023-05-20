adbfbead7ba2ffb61ff109fb4a439e8f9dabde81
Pratyush Singh <aries.pratyush@gmail.com>
2023-05-20 20:14:58 +0530

    migrated activities from butterknife to viewbinding

--- app/src/main/java/org/mifos/mobile/ui/activities/UserProfileActivity.kt
+++ app/src/main/java/org/mifos/mobile/ui/activities/UserProfileActivity.kt
@@ -3,14 +3,18 @@ package org.mifos.mobile.ui.activities
 import android.os.Bundle
 
 import org.mifos.mobile.R
+import org.mifos.mobile.databinding.ActivityUserProfileBinding
 import org.mifos.mobile.ui.activities.base.BaseActivity
 import org.mifos.mobile.ui.fragments.UserProfileFragment
 
 class UserProfileActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityUserProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_profile)
+        binding = ActivityUserProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         replaceFragment(UserProfileFragment.newInstance(), false, R.id.container)
     }
 }
