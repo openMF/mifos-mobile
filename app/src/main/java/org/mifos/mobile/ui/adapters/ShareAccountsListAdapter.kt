20d817084fdcfb1ae62621531ef515e27ca8d95d
Pratyush Singh <aries.pratyush@gmail.com>
2023-05-27 19:15:50 +0530

    fix #2142: migrated adapters to viewbinding

--- app/src/main/java/org/mifos/mobile/ui/adapters/ShareAccountsListAdapter.kt
+++ app/src/main/java/org/mifos/mobile/ui/adapters/ShareAccountsListAdapter.kt
@@ -1,32 +1,20 @@
 package org.mifos.mobile.ui.adapters
 
-import android.content.Context
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.LayerDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
-
-import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-import butterknife.BindView
-import butterknife.ButterKnife
-
 import org.mifos.mobile.R
-import org.mifos.mobile.injection.ActivityContext
+import org.mifos.mobile.databinding.RowShareAccountBinding
 import org.mifos.mobile.models.accounts.share.ShareAccount
 
 import java.util.*
-import javax.inject.Inject
 
 class ShareAccountsListAdapter (
-    val onItemClick: (itemPosition: Int) -> Unit
-):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val onItemClick: (itemPosition: Int) -> Unit
+):RecyclerView.Adapter<ShareAccountsListAdapter.ViewHolder>() {
 
     private var shareAccountsList: List<ShareAccount?>? = ArrayList()
     fun setShareAccountsList(shareAccountsList: List<ShareAccount?>?) {
@@ -39,48 +27,14 @@ class ShareAccountsListAdapter (
         return shareAccountsList?.get(position)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val vh: RecyclerView.ViewHolder
-        val v = LayoutInflater.from(parent.context).inflate(
-                R.layout.row_share_account, parent, false)
-        vh = ViewHolder(v)
-        v.setOnClickListener {
-            onItemClick(vh.bindingAdapterPosition)
-        }
-        return vh
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = RowShareAccountBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is ViewHolder) {
-            val shareAccount = getItem(position)
-            holder.tvClientShareAccountsNumber?.text = shareAccount?.accountNo
-            holder.tvShareAccountsProductName?.text = shareAccount?.productName
-            holder.llAccountDetail?.visibility = View.GONE
-            val context = holder.itemView.context
-            when {
-                shareAccount?.status?.active == true -> {
-                    holder.ivStatusIndicator?.setColorFilter(ContextCompat.getColor(context, R.color.deposit_green))
-                    setSharingAccountDetail(holder, shareAccount)
-                }
-                shareAccount?.status?.approved == true -> {
-                    holder.ivStatusIndicator?.setColorFilter(ContextCompat.getColor(context, R.color.light_green))
-                }
-                shareAccount?.status?.submittedAndPendingApproval == true -> {
-                    holder.ivStatusIndicator?.setColorFilter(ContextCompat.getColor(context, R.color.light_yellow))
-                }
-                else -> {
-                    holder.ivStatusIndicator?.setColorFilter(ContextCompat.getColor(context, R.color.light_blue))
-                }
-            }
-        }
-    }
-
-    private fun setSharingAccountDetail(viewHolder: ViewHolder, shareAccount: ShareAccount) {
-        viewHolder.llAccountDetail?.visibility = View.VISIBLE
-        viewHolder.tvSharesPending?.text = shareAccount
-                .totalPendingForApprovalShares.toString()
-        viewHolder.tvSharesApproved?.text = shareAccount
-                .totalApprovedShares.toString()
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val shareAccount = getItem(position)
+        holder.bind(shareAccount)
     }
 
     override fun getItemCount(): Int {
@@ -89,34 +43,46 @@ class ShareAccountsListAdapter (
         return 0
     }
 
-
-    class ViewHolder(v: View?) : RecyclerView.ViewHolder(v!!) {
-        @JvmField
-        @BindView(R.id.tv_clientSharingAccountNumber)
-        var tvClientShareAccountsNumber: TextView? = null
-
-        @JvmField
-        @BindView(R.id.tv_shareAccountProductName)
-        var tvShareAccountsProductName: TextView? = null
-
-        @JvmField
-        @BindView(R.id.iv_status_indicator)
-        var ivStatusIndicator: AppCompatImageView? = null
-
-        @JvmField
-        @BindView(R.id.ll_account_detail)
-        var llAccountDetail: LinearLayout? = null
-
-        @JvmField
-        @BindView(R.id.tv_shares_pending)
-        var tvSharesPending: TextView? = null
-
-        @JvmField
-        @BindView(R.id.tv_shares_approved)
-        var tvSharesApproved: TextView? = null
+    inner class ViewHolder(private val binding: RowShareAccountBinding) : RecyclerView.ViewHolder(binding.root) {
 
         init {
-            ButterKnife.bind(this, v!!)
+            binding.root.setOnClickListener {
+                onItemClick(bindingAdapterPosition)
+            }
+        }
+
+        fun bind(shareAccount: ShareAccount?) {
+            with(binding) {
+                tvClientSharingAccountNumber.text = shareAccount?.accountNo
+                tvShareAccountProductName.text = shareAccount?.productName
+                llAccountDetail.visibility = View.GONE
+                val context = itemView.context
+                when {
+                    shareAccount?.status?.active == true -> {
+                        ivStatusIndicator.setColorFilter(ContextCompat.getColor(context, R.color.deposit_green))
+                        setSharingAccountDetail(shareAccount)
+                    }
+                    shareAccount?.status?.approved == true -> {
+                        ivStatusIndicator.setColorFilter(ContextCompat.getColor(context, R.color.light_green))
+                    }
+                    shareAccount?.status?.submittedAndPendingApproval == true -> {
+                        ivStatusIndicator.setColorFilter(ContextCompat.getColor(context, R.color.light_yellow))
+                    }
+                    else -> {
+                        ivStatusIndicator.setColorFilter(ContextCompat.getColor(context, R.color.light_blue))
+                    }
+                }
+            }
+        }
+
+        private fun setSharingAccountDetail(shareAccount: ShareAccount) {
+            with(binding) {
+                llAccountDetail.visibility = View.VISIBLE
+                tvSharesPending.text = shareAccount
+                    .totalPendingForApprovalShares.toString()
+                tvSharesApproved.text = shareAccount
+                    .totalApprovedShares.toString()
+            }
         }
     }
 }
