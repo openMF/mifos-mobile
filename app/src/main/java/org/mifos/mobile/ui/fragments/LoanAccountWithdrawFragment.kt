a092977a0951bbd0bd4caa84778622ccf3a73253
Pratyush Singh <aries.pratyush@gmail.com>
2023-05-27 16:02:21 +0530

    fix #2142: migrated savings,update,user and loan fragments to viewbinding

--- app/src/main/java/org/mifos/mobile/ui/fragments/LoanAccountWithdrawFragment.kt
+++ app/src/main/java/org/mifos/mobile/ui/fragments/LoanAccountWithdrawFragment.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
-
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
 
 import org.mifos.mobile.R
+import org.mifos.mobile.databinding.FragmentLoanWithdrawBinding
 import org.mifos.mobile.models.accounts.loan.LoanWithAssociations
 import org.mifos.mobile.models.accounts.loan.LoanWithdraw
 import org.mifos.mobile.presenters.LoanAccountWithdrawPresenter
@@ -29,22 +24,12 @@ import javax.inject.Inject
  */
 class LoanAccountWithdrawFragment : BaseFragment(), LoanAccountWithdrawView {
 
-    @kotlin.jvm.JvmField
-    @BindView(R.id.tv_client_name)
-    var tvClientName: TextView? = null
-
-    @kotlin.jvm.JvmField
-    @BindView(R.id.tv_account_number)
-    var tvAccountNumber: TextView? = null
-
-    @kotlin.jvm.JvmField
-    @BindView(R.id.et_withdraw_reason)
-    var etWithdrawReason: EditText? = null
+    private var _binding: FragmentLoanWithdrawBinding? = null
+    private val binding get() = _binding!!
 
     @kotlin.jvm.JvmField
     @Inject
     var loanAccountWithdrawPresenter: LoanAccountWithdrawPresenter? = null
-    var rootView: View? = null
     private var loanWithAssociations: LoanWithAssociations? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,29 +43,34 @@ class LoanAccountWithdrawFragment : BaseFragment(), LoanAccountWithdrawView {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        rootView = inflater.inflate(R.layout.fragment_loan_withdraw, container, false)
+        _binding = FragmentLoanWithdrawBinding.inflate(inflater,container,false)
         setToolbarTitle(getString(R.string.withdraw_loan))
-        ButterKnife.bind(this, rootView!!)
         showUserInterface()
         loanAccountWithdrawPresenter?.attachView(this)
-        return rootView
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnWithdrawLoan.setOnClickListener {
+            onLoanWithdraw()
+        }
     }
 
     /**
      * Sets Basic Information about that Loan Application
      */
     private fun showUserInterface() {
-        tvClientName?.text = loanWithAssociations?.clientName
-        tvAccountNumber?.text = loanWithAssociations?.accountNo
+        binding.tvClientName.text = loanWithAssociations?.clientName
+        binding.tvAccountNumber.text = loanWithAssociations?.accountNo
     }
 
     /**
      * Sends a request to server to withdraw that Loan Account
      */
-    @OnClick(R.id.btn_withdraw_loan)
     fun onLoanWithdraw() {
         val loanWithdraw = LoanWithdraw()
-        loanWithdraw.note = etWithdrawReason?.text.toString()
+        loanWithdraw.note = binding.etWithdrawReason.text.toString()
         loanWithdraw.withdrawnOnDate = DateHelper
                 .getDateAsStringFromLong(System.currentTimeMillis())
         loanAccountWithdrawPresenter?.withdrawLoanAccount(loanWithAssociations?.id?.toLong(),
@@ -91,7 +81,7 @@ class LoanAccountWithdrawFragment : BaseFragment(), LoanAccountWithdrawView {
      * Receives A confirmation after successfull withdrawing of Loan Application.
      */
     override fun showLoanAccountWithdrawSuccess() {
-        Toaster.show(rootView, R.string.loan_application_withdrawn_successfully)
+        Toaster.show(binding.root, R.string.loan_application_withdrawn_successfully)
         activity?.supportFragmentManager?.popBackStack()
     }
 
@@ -102,7 +92,7 @@ class LoanAccountWithdrawFragment : BaseFragment(), LoanAccountWithdrawView {
      * @param message Error Message displayed
      */
     override fun showLoanAccountWithdrawError(message: String?) {
-        Toaster.show(rootView, message)
+        Toaster.show(binding.root, message)
     }
 
     override fun showProgress() {
@@ -117,6 +107,7 @@ class LoanAccountWithdrawFragment : BaseFragment(), LoanAccountWithdrawView {
         super.onDestroyView()
         hideProgress()
         loanAccountWithdrawPresenter?.detachView()
+        _binding = null
     }
 
     companion object {
--- app/src/main/java/org/mifos/mobile/ui/fragments/UpdatePasswordFragment.kt
+++ app/src/main/java/org/mifos/mobile/ui/fragments/UpdatePasswordFragment.kt
@@ -1,6 +1,5 @@
 package org.mifos.mobile.ui.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -9,15 +8,11 @@ import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.view.ViewGroup
 import android.widget.Toast
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
-import com.google.android.material.textfield.TextInputLayout
 import org.mifos.mobile.R
 import org.mifos.mobile.api.local.PreferencesHelper
+import org.mifos.mobile.databinding.FragmentUpdatePasswordBinding
 import org.mifos.mobile.models.UpdatePasswordPayload
 import org.mifos.mobile.presenters.UpdatePasswordPresenter
-import org.mifos.mobile.ui.activities.SettingsActivity
 import org.mifos.mobile.ui.activities.base.BaseActivity
 import org.mifos.mobile.ui.fragments.base.BaseFragment
 import org.mifos.mobile.ui.views.UpdatePasswordView
@@ -28,13 +23,10 @@ import javax.inject.Inject
 /*
 * Created by saksham on 13/July/2018
 */   class UpdatePasswordFragment : BaseFragment(), UpdatePasswordView, TextWatcher, OnFocusChangeListener {
-    @kotlin.jvm.JvmField
-    @BindView(R.id.til_new_password)
-    var tilNewPassword: TextInputLayout? = null
 
-    @kotlin.jvm.JvmField
-    @BindView(R.id.til_confirm_new_password)
-    var tilConfirmNewPassword: TextInputLayout? = null
+    private var _binding: FragmentUpdatePasswordBinding? = null
+    private val binding get() = _binding!!
+
 
     @kotlin.jvm.JvmField
     @Inject
@@ -43,7 +35,6 @@ import javax.inject.Inject
     @kotlin.jvm.JvmField
     @Inject
     var preferencesHelper: PreferencesHelper? = null
-    private var rootView: View? = null
     private var payload: UpdatePasswordPayload? = null
     private var isFocusLostNewPassword = false
     private var isFocusLostConfirmPassword = false
@@ -52,19 +43,24 @@ import javax.inject.Inject
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        rootView = inflater.inflate(R.layout.fragment_update_password, container, false)
+        _binding = FragmentUpdatePasswordBinding.inflate(inflater,container,false)
         setToolbarTitle(getString(R.string.change_password))
-        ButterKnife.bind(this, rootView!!)
         (activity as BaseActivity?)?.activityComponent?.inject(this)
         presenter?.attachView(this)
-        tilNewPassword?.editText?.addTextChangedListener(this)
-        tilConfirmNewPassword?.editText?.addTextChangedListener(this)
-        tilNewPassword?.editText?.onFocusChangeListener = this
-        tilConfirmNewPassword?.editText?.onFocusChangeListener = this
-        return rootView
+        binding.tilNewPassword.editText?.addTextChangedListener(this)
+        binding.tilConfirmNewPassword.editText?.addTextChangedListener(this)
+        binding.tilNewPassword.editText?.onFocusChangeListener = this
+        binding.tilConfirmNewPassword.editText?.onFocusChangeListener = this
+        return binding.root
     }
 
-    @OnClick(R.id.btn_update_password)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnUpdatePassword.setOnClickListener {
+            updatePassword()
+        }
+    }
+
     fun updatePassword() {
         if (isFieldsCompleted) {
             presenter?.updateAccountPassword(updatePasswordPayload)
@@ -74,8 +70,8 @@ import javax.inject.Inject
     private val isFieldsCompleted: Boolean
         get() {
             var rv = true
-            val newPassword = tilNewPassword?.editText?.text.toString().trim { it <= ' ' }
-            val repeatPassword = tilConfirmNewPassword?.editText?.text.toString().trim { it <= ' ' }
+            val newPassword = binding.tilNewPassword.editText?.text.toString().trim { it <= ' ' }
+            val repeatPassword = binding.tilConfirmNewPassword.editText?.text.toString().trim { it <= ' ' }
             if (!checkNewPasswordFieldsComplete()) {
                 rv = false
             }
@@ -83,7 +79,7 @@ import javax.inject.Inject
                 rv = false
             }
             if (newPassword != repeatPassword) {
-                Toaster.show(rootView, getString(R.string.error_password_not_match))
+                Toaster.show(binding.root, getString(R.string.error_password_not_match))
                 rv = false
             }
             return rv
@@ -91,8 +87,8 @@ import javax.inject.Inject
     private val updatePasswordPayload: UpdatePasswordPayload?
         get() {
             payload = UpdatePasswordPayload()
-            payload?.password = tilNewPassword?.editText?.text.toString().trim { it <= ' ' }
-            payload?.repeatPassword = tilConfirmNewPassword?.editText?.text.toString().trim { it <= ' ' }
+            payload?.password = binding.tilNewPassword.editText?.text.toString().trim { it <= ' ' }
+            payload?.repeatPassword = binding.tilConfirmNewPassword.editText?.text.toString().trim { it <= ' ' }
             return payload
         }
 
@@ -101,7 +97,7 @@ import javax.inject.Inject
         if (!Network.isConnected(activity)) {
             message = getString(R.string.no_internet_connection)
         }
-        Toaster.show(rootView, message)
+        Toaster.show(binding.root, message)
     }
 
     override fun showPasswordUpdatedSuccessfully() {
@@ -122,14 +118,15 @@ import javax.inject.Inject
     override fun onDestroyView() {
         super.onDestroyView()
         presenter?.detachView()
+        _binding = null
     }
 
     override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
     override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        if (tilNewPassword?.editText?.hasFocus() == true && isFocusLostNewPassword) {
+        if (binding.tilNewPassword.editText?.hasFocus() == true && isFocusLostNewPassword) {
             checkNewPasswordFieldsComplete()
         }
-        if (tilConfirmNewPassword?.editText?.hasFocus() == true && isFocusLostConfirmPassword) {
+        if (binding.tilConfirmNewPassword.editText?.hasFocus() == true && isFocusLostConfirmPassword) {
             checkConfirmPasswordFieldsComplete()
         }
     }
@@ -147,41 +144,44 @@ import javax.inject.Inject
     }
 
     private fun checkNewPasswordFieldsComplete(): Boolean {
-        val newPassword = tilNewPassword?.editText?.text.toString()
+        val newPassword = binding.tilNewPassword.editText?.text.toString()
         isFocusLostNewPassword = true
         if (newPassword.isEmpty()) {
-            tilNewPassword?.error = getString(R.string.error_validation_blank,
+            binding.tilNewPassword.error = getString(R.string.error_validation_blank,
                     getString(R.string.new_password))
             return false
         }
         if (newPassword.length < 6) {
-            tilNewPassword?.error = getString(R.string.error_validation_minimum_chars,
+            binding.tilNewPassword.error = getString(R.string.error_validation_minimum_chars,
                     getString(R.string.new_password),
                     resources.getInteger(R.integer.password_minimum_length))
             return false
         }
-        tilNewPassword?.isErrorEnabled = false
+        binding.tilNewPassword.isErrorEnabled = false
         return true
     }
 
     private fun checkConfirmPasswordFieldsComplete(): Boolean {
-        val confirmPassword = tilConfirmNewPassword?.editText?.text.toString()
+        val confirmPassword = binding.tilConfirmNewPassword.editText?.text.toString()
         isFocusLostConfirmPassword = true
         if (confirmPassword.isEmpty()) {
-            tilConfirmNewPassword?.error = getString(R.string.error_validation_blank,
+            binding.tilConfirmNewPassword.error = getString(R.string.error_validation_blank,
                     getString(R.string.confirm_password))
             return false
         }
         if (confirmPassword.length < 6) {
-            tilConfirmNewPassword?.error = getString(R.string.error_validation_minimum_chars,
+            binding.tilConfirmNewPassword.error = getString(R.string.error_validation_minimum_chars,
                     getString(R.string.confirm_password),
                     resources.getInteger(R.integer.password_minimum_length))
             return false
         }
-        tilConfirmNewPassword?.isErrorEnabled = false
+        binding.tilConfirmNewPassword.isErrorEnabled = false
         return true
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+    }
     companion object {
         @kotlin.jvm.JvmStatic
         fun newInstance(): UpdatePasswordFragment {
