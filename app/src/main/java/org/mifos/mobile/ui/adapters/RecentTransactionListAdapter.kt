a298ccc261f2c3dd5535f667773201e2c67d7dca
gururani-abhishek <gururani.abhishek@gmail.com>
2023-06-07 11:09:05 +0530

    fix #2142 : migrating RecentTransactionListAdapter to ViewBinding

--- app/src/main/java/org/mifos/mobile/ui/adapters/RecentTransactionListAdapter.kt
+++ app/src/main/java/org/mifos/mobile/ui/adapters/RecentTransactionListAdapter.kt
@@ -2,17 +2,16 @@ package org.mifos.mobile.ui.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-import butterknife.BindView
-import butterknife.ButterKnife
+import org.mifos.mobile.MifosSelfServiceApp.Companion.context
 
 import org.mifos.mobile.R
+import org.mifos.mobile.databinding.RowRecentTransactionBinding
 import org.mifos.mobile.injection.ActivityContext
 import org.mifos.mobile.models.Transaction
+import org.mifos.mobile.models.client.Type
 import org.mifos.mobile.utils.CurrencyUtil.formatCurrency
 import org.mifos.mobile.utils.DateHelper.getDateAsString
 import org.mifos.mobile.utils.Utils.formatTransactionType
@@ -31,9 +30,8 @@ class RecentTransactionListAdapter @Inject constructor(@ActivityContext context:
     private val context: Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.row_recent_transaction, parent, false)
-        return ViewHolder(view)
+        val binding = RowRecentTransactionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -42,11 +40,8 @@ class RecentTransactionListAdapter @Inject constructor(@ActivityContext context:
         if (currencyRepresentation == null) {
             currencyRepresentation = currency?.code
         }
-        holder.tvAmount?.text = context.getString(R.string.string_and_string,
-                currencyRepresentation, formatCurrency(context,
-                amount!!))
-        holder.tvTypeValue?.text = formatTransactionType(type.value)
-        holder.tvTransactionsDate?.text = getDateAsString(submittedOnDate)
+
+        holder.bind(currencyRepresentation, type, submittedOnDate, amount)
     }
 
     fun setTransactions(transactions: MutableList<Transaction?>?) {
@@ -64,21 +59,15 @@ class RecentTransactionListAdapter @Inject constructor(@ActivityContext context:
         return transactions.size
     }
 
-    class ViewHolder(v: View?) : RecyclerView.ViewHolder(v!!) {
-        @JvmField
-        @BindView(R.id.tv_transactionDate)
-        var tvTransactionsDate: TextView? = null
-
-        @JvmField
-        @BindView(R.id.tv_amount)
-        var tvAmount: TextView? = null
-
-        @JvmField
-        @BindView(R.id.tv_value)
-        var tvTypeValue: TextView? = null
-
-        init {
-            ButterKnife.bind(this, v!!)
+    class ViewHolder(private val binding : RowRecentTransactionBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(currencyRepresentation: String?, type: Type, submittedOnDate: List<Int>, amount: Double?) {
+            with(binding) {
+                tvAmount.text = context?.getString(R.string.string_and_string,
+                    currencyRepresentation, formatCurrency(context,
+                        amount!!))
+                tvValue.text = formatTransactionType(type.value)
+                tvTransactionDate.text = getDateAsString(submittedOnDate)
+            }
         }
     }
 
