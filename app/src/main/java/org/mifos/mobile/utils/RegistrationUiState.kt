f32cdf912a2213ad136846083790f5445d99dc62
gururani-abhishek <gururani.abhishek@gmail.com>
2023-06-22 11:46:37 +0530

    feat : migration of Registration Fragment to MVVM.

---
+++ app/src/main/java/org/mifos/mobile/utils/RegistrationUiState.kt
@@ -0,0 +1,7 @@
+package org.mifos.mobile.utils
+
+sealed class RegistrationUiState {
+    data class ErrorOnRegistration(val exception : Throwable) : RegistrationUiState()
+    object RegistrationSuccessful : RegistrationUiState()
+    object Loading : RegistrationUiState()
+}
