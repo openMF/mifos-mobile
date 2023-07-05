cf17b4ff033c1cc59a99e885ed879d2d3bb84db0
Pratyush Singh <aries.pratyush@gmail.com>
2023-07-05 23:04:04 +0530

    feat: transfer process fragment migrated to mvvm

--- app/src/main/java/org/mifos/mobile/injection/module/RepositoryModule.kt
+++ app/src/main/java/org/mifos/mobile/injection/module/RepositoryModule.kt
@@ -5,6 +5,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.mifos.mobile.api.DataManager
+import org.mifos.mobile.repositories.TransferRepository
+import org.mifos.mobile.repositories.TransferRepositoryImp
 import org.mifos.mobile.repositories.UserAuthRepository
 import org.mifos.mobile.repositories.UserAuthRepositoryImp
 
@@ -15,5 +17,10 @@ class RepositoryModule {
     @Provides
     fun providesUserAuthRepository(dataManager: DataManager): UserAuthRepository {
         return UserAuthRepositoryImp(dataManager)
+    }
+
+    @Provides
+    fun providesTransferRepository(dataManager: DataManager): TransferRepository {
+        return TransferRepositoryImp(dataManager)
     }
 }
---
+++ app/src/main/java/org/mifos/mobile/repositories/TransferRepositoryImp.kt
@@ -0,0 +1,47 @@
+package org.mifos.mobile.repositories
+
+import io.reactivex.Observable
+import okhttp3.ResponseBody
+import org.mifos.mobile.api.DataManager
+import org.mifos.mobile.models.payload.TransferPayload
+import javax.inject.Inject
+
+class TransferRepositoryImp @Inject constructor(private val dataManager: DataManager) :
+    TransferRepository {
+    override fun makeTransfer(
+        fromOfficeId: Int?,
+        fromClientId: Long?,
+        fromAccountType: Int?,
+        fromAccountId: Int?,
+        toOfficeId: Int?,
+        toClientId: Long?,
+        toAccountType: Int?,
+        toAccountId: Int?,
+        transferDate: String?,
+        transferAmount: Double?,
+        transferDescription: String?,
+        dateFormat: String,
+        locale: String,
+        fromAccountNumber: String?,
+        toAccountNumber: String?
+    ): Observable<ResponseBody?>? {
+        val transferPayload = TransferPayload().apply {
+            this.fromOfficeId = fromOfficeId
+            this.fromClientId = fromClientId
+            this.fromAccountType = fromAccountType
+            this.fromAccountId = fromAccountId
+            this.toOfficeId = toOfficeId
+            this.toClientId = toClientId
+            this.toAccountType = toAccountType
+            this.toAccountId = toAccountId
+            this.transferDate = transferDate
+            this.transferAmount = transferAmount
+            this.transferDescription = transferDescription
+            this.dateFormat = dateFormat
+            this.locale = locale
+            this.fromAccountNumber = fromAccountNumber
+            this.toAccountNumber = toAccountNumber
+        }
+        return dataManager.makeTransfer(transferPayload)
+    }
+}
