b964842235dd210b8e28e5757b234ce83f3f9eb0
gururani-abhishek <gururani.abhishek@gmail.com>
2023-07-07 11:27:17 +0530

    feat : login activity mvvm migration

--- app/src/main/java/org/mifos/mobile/repositories/UserAuthRepository.kt
+++ app/src/main/java/org/mifos/mobile/repositories/UserAuthRepository.kt
@@ -2,6 +2,9 @@ package org.mifos.mobile.repositories
 
 import io.reactivex.Observable
 import okhttp3.ResponseBody
+import org.mifos.mobile.models.Page
+import org.mifos.mobile.models.User
+import org.mifos.mobile.models.client.Client
 
 interface UserAuthRepository {
 
@@ -15,4 +18,17 @@ interface UserAuthRepository {
         password: String?,
         username: String?
     ): Observable<ResponseBody?>?
+
+    fun login(username: String, password: String): Observable<User?>?
+
+    fun loadClient() : Observable<Page<Client?>?>?
+
+    fun saveAuthenticationTokenForSession(user: User)
+
+    fun reInitializeService()
+
+    fun setClientId(clientId: Long?)
+
+    fun clearPrefHelper()
+
 }
