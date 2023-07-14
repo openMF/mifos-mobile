b2997f3e0262e37100083ba717746651103a29f9
Pratyush Singh <aries.pratyush@gmail.com>
2023-07-15 01:08:31 +0530

    feat: migrated loan related presenter to mvvm

--- app/src/main/java/org/mifos/mobile/injection/module/RepositoryModule.kt
+++ app/src/main/java/org/mifos/mobile/injection/module/RepositoryModule.kt
@@ -5,6 +5,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.mifos.mobile.api.DataManager
+import org.mifos.mobile.repositories.LoanRepository
+import org.mifos.mobile.repositories.LoanRepositoryImp
 import org.mifos.mobile.repositories.UserAuthRepository
 import org.mifos.mobile.repositories.UserAuthRepositoryImp
 
@@ -15,5 +17,10 @@ class RepositoryModule {
     @Provides
     fun providesUserAuthRepository(dataManager: DataManager): UserAuthRepository {
         return UserAuthRepositoryImp(dataManager)
+    }
+
+    @Provides
+    fun providesLoanRepository(dataManager: DataManager): LoanRepository {
+        return LoanRepositoryImp(dataManager)
     }
 }
