c807068a7238a081f3f0aac8a422099f335d9a75
Pratyush Singh <aries.pratyush@gmail.com>
2023-07-24 23:33:43 +0530

    feat: QrCodeImport migrated to MVVM

---
+++ app/src/main/java/org/mifos/mobile/utils/QrCodeUiState.kt
@@ -0,0 +1,9 @@
+package org.mifos.mobile.utils
+
+import com.google.zxing.Result
+
+sealed class QrCodeUiState {
+    object Loading : QrCodeUiState()
+    data class ShowError(val message: Int) : QrCodeUiState()
+    data class HandleDecodedResult(val result: Result?) : QrCodeUiState()
+}
