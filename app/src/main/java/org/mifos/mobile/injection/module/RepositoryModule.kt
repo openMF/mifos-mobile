337d374f95ba005d3349f1534ede4592ad51d780
gururani-abhishek <gururani.abhishek@gmail.com>
2023-08-01 13:35:15 +0530

    feat : savingFeatures-MVVM-Migration

--- app/src/main/java/org/mifos/mobile/injection/module/RepositoryModule.kt
+++ app/src/main/java/org/mifos/mobile/injection/module/RepositoryModule.kt
@@ -4,20 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.mifos.mobile.repositories.NotificationRepositoryImp
 import org.mifos.mobile.api.DataManager
-import org.mifos.mobile.repositories.SavingsAccountRepository
-import org.mifos.mobile.repositories.SavingsAccountRepositoryImp
-import org.mifos.mobile.repositories.LoanRepository
-import org.mifos.mobile.repositories.LoanRepositoryImp
-import org.mifos.mobile.repositories.NotificationRepository
 import org.mifos.mobile.api.local.PreferencesHelper
-import org.mifos.mobile.repositories.ClientRepository
-import org.mifos.mobile.repositories.ClientRepositoryImp
-import org.mifos.mobile.repositories.RecentTransactionRepository
-import org.mifos.mobile.repositories.RecentTransactionRepositoryImp
-import org.mifos.mobile.repositories.UserAuthRepository
-import org.mifos.mobile.repositories.UserAuthRepositoryImp
 import org.mifos.mobile.repositories.*
 
 @Module
