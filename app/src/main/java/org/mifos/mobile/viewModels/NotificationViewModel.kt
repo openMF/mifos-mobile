490fea154e87ffb1a4b156bc900eece882b591dd
Pratyush Singh <aries.pratyush@gmail.com>
2023-08-16 20:19:45 +0530

    feat: implemented coroutines

--- app/src/main/java/org/mifos/mobile/viewModels/NotificationViewModel.kt
+++ app/src/main/java/org/mifos/mobile/viewModels/NotificationViewModel.kt
@@ -3,41 +3,30 @@ package org.mifos.mobile.viewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.observers.DisposableObserver
-import io.reactivex.schedulers.Schedulers
-import org.mifos.mobile.utils.NotificationUiState
-import org.mifos.mobile.models.notification.MifosNotification
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 import org.mifos.mobile.repositories.NotificationRepository
+import org.mifos.mobile.utils.NotificationUiState
 import javax.inject.Inject
 
 @HiltViewModel
-class NotificationViewModel @Inject constructor(private val notificationRepositoryImp: NotificationRepository): ViewModel() {
-    private val compositeDisposables: CompositeDisposable = CompositeDisposable()
+class NotificationViewModel @Inject constructor(private val notificationRepositoryImp: NotificationRepository) :
+    ViewModel() {
 
     private val _notificationUiState = MutableLiveData<NotificationUiState>()
-    val notificationUiState : LiveData<NotificationUiState> get() = _notificationUiState
+    val notificationUiState: LiveData<NotificationUiState> get() = _notificationUiState
 
     fun loadNotifications() {
         _notificationUiState.value = NotificationUiState.Loading
-        notificationRepositoryImp.loadNotifications()
-            .observeOn(AndroidSchedulers.mainThread())
-            ?.subscribeOn(Schedulers.io())
-            ?.subscribeWith(object : DisposableObserver<List<MifosNotification?>?>() {
-                override fun onComplete() {}
-                override fun onError(e: Throwable) {
-                    _notificationUiState.value = NotificationUiState.Error
-                }
-                override fun onNext(notificationModels: List<MifosNotification?>) {
-                    _notificationUiState.value = NotificationUiState.LoadNotificationsSuccessful(notificationModels)
-                }
-            })?.let { compositeDisposables.add(it) }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposables.clear()
+        viewModelScope.launch {
+            notificationRepositoryImp.loadNotifications().catch {
+                _notificationUiState.value = NotificationUiState.Error
+            }.collect { list ->
+                _notificationUiState.value =
+                    list?.let { NotificationUiState.LoadNotificationsSuccessful(it) }
+            }
+        }
     }
 }
