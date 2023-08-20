0b1cebb7347cc6a3e8c89bcb7f0dd19e5981bb69
Pratyush Singh <aries.pratyush@gmail.com>
2023-08-21 02:05:03 +0530

    feat: dark & light mode in compose view

---
+++ app/src/main/java/org/mifos/mobile/theme/Theme.kt
@@ -0,0 +1,46 @@
+package org.mifos.mobile.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightThemeColors = lightColors(
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Black2,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    onSecondary = Color.Black,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = BackgroundLight,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Black2,
+)
+
+private val DarkThemeColors = darkColors(
+    primary = Blue700,
+    primaryVariant = Color.White,
+    onPrimary = Color.White,
+    secondary = Black1,
+    onSecondary = Color.White,
+    error = RedErrorLight,
+    background = BackgroundDark,
+    onBackground = Color.White,
+    surface = Black1,
+    onSurface = Color.White,
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean,
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkThemeColors else LightThemeColors,
+        content = content
+    )
+}
