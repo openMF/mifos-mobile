6a026b224d485cc1c8c4274bc78a6e03fe8c5d43
Pratyush Singh <aries.pratyush@gmail.com>
2023-08-25 19:03:55 +0530

    refactor: navigation via fragments

--- app/src/main/java/org/mifos/mobile/ui/about/AboutUsScreen.kt
+++ app/src/main/java/org/mifos/mobile/ui/about/AboutUsScreen.kt
@@ -15,85 +15,77 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import org.mifos.mobile.R
 import org.mifos.mobile.core.ui.component.CopyrightItemContent
 import org.mifos.mobile.core.ui.component.MifosItemCard
 import org.mifos.mobile.core.ui.component.MifosItemContent
-import org.mifos.mobile.core.ui.theme.MifosMobileTheme
-import org.mifos.mobile.ui.activities.PrivacyPolicyActivity
-import org.mifos.mobile.utils.Constants.LICENSE_LINK
-import org.mifos.mobile.utils.Constants.SOURCE_CODE_LINK
-import org.mifos.mobile.utils.Constants.WEBSITE_LINK
-import org.mifos.mobile.utils.StartActivity
+import org.mifos.mobile.ui.enums.AboutUsListItemId
 import java.util.*
 
 @SuppressWarnings("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AboutUsScreen(viewModel: AboutUsViewModel = viewModel()) {
-    val context = StartActivity(LocalContext.current)
 
-    MifosMobileTheme {
-        Scaffold {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp, 56.dp, 16.dp, 16.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.mipmap.mifos_icon),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(100.dp)
-                        .fillMaxWidth()
-                        .align(Alignment.CenterHorizontally)
-                )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp, 56.dp, 16.dp, 16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.mipmap.mifos_icon),
+            contentDescription = null,
+            modifier = Modifier
+                .size(100.dp)
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
 
-                Text(
-                    text = stringResource(id = R.string.app_name),
-                    style = MaterialTheme.typography.headlineMedium,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 8.dp)
-                )
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.headlineMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        )
 
-                Text(
-                    text = stringResource(id = R.string.about_app_description),
-                    style = MaterialTheme.typography.titleSmall.copy(),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 16.dp)
-                )
+        Text(
+            text = stringResource(id = R.string.about_app_description),
+            style = MaterialTheme.typography.titleSmall.copy(),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
 
-                LazyColumn {
-                    items(viewModel.aboutUsList) { item ->
-                        MifosItemCard(onClick = {
-                            when (item.name) {
-                                R.string.official_website -> {
-                                    context.startActivity(WEBSITE_LINK)
-                                }
-                                R.string.licenses -> {
-                                    context.startActivity(LICENSE_LINK)
-                                }
-                                R.string.privacy_policy -> {
-                                    context.startActivity(PrivacyPolicyActivity::class.java)
-                                }
-                                R.string.sources -> {
-                                    context.startActivity(SOURCE_CODE_LINK)
-                                }
-                                R.string.license_string_with_value -> {
-                                    context.startActivity(OssLicensesMenuActivity::class.java)
-                                }
+        LazyColumn {
+            items(viewModel.aboutUsItems) { item ->
+                MifosItemCard(
+                    onClick = {
+                        when (item.itemId) {
+                            AboutUsListItemId.OFFICE_WEBSITE -> {
+                                viewModel.navigateToItem(AboutUsListItemId.OFFICE_WEBSITE)
                             }
-                        }) {
-                            AboutUsItemCard(
-                                name = item.name,
-                                iconUrl = item.iconUrl
-                            )
+                            AboutUsListItemId.LICENSES -> {
+                                viewModel.navigateToItem(AboutUsListItemId.LICENSES)
+                            }
+                            AboutUsListItemId.PRIVACY_POLICY -> {
+                                viewModel.navigateToItem(AboutUsListItemId.PRIVACY_POLICY)
+                            }
+                            AboutUsListItemId.SOURCE_CODE -> {
+                                viewModel.navigateToItem(AboutUsListItemId.SOURCE_CODE)
+                            }
+                            AboutUsListItemId.LICENSES_STRING_WITH_VALUE -> {
+                                viewModel.navigateToItem(AboutUsListItemId.LICENSES_STRING_WITH_VALUE)
+                            }
+                            else -> {}
                         }
                     }
+                ) {
+                    AboutUsItemCard(
+                        name = item.name,
+                        iconUrl = item.iconUrl
+                    )
                 }
             }
         }
