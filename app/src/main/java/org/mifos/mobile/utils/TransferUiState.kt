8d995cedc8ba202d5c50fad6cc6cb1450c30465b
Pratyush Singh <aries.pratyush@gmail.com>
2023-10-24 01:29:26 +0530

    refactor #2415: transfer related viewmodels with stateflow

--- app/src/main/java/org/mifos/mobile/utils/TransferUiState.kt
+++ app/src/main/java/org/mifos/mobile/utils/TransferUiState.kt
@@ -1,6 +1,7 @@
 package org.mifos.mobile.utils
 
 sealed class TransferUiState {
+    object Initial : TransferUiState()
     object Loading : TransferUiState()
     object TransferSuccess : TransferUiState()
     data class Error(val errorMessage: Throwable) : TransferUiState()
