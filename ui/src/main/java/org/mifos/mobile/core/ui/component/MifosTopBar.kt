fdbadf5fda78b6352c37747f1df5159ad704cb09
Avneet Singh <avneetmaankiya@gmail.com>
2024-01-22 06:18:40 +0530

    refactor #2447: migrated help screen to compose

---
+++ ui/src/main/java/org/mifos/mobile/core/ui/component/MifosTopBar.kt
@@ -0,0 +1,44 @@
+package org.mifos.mobile.core.ui.component
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MifosTopBar(
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit,
+    title: @Composable () -> Unit
+) {
+    TopAppBar(
+        modifier = Modifier,
+        title = title,
+        navigationIcon = {
+            IconButton(
+                onClick = { navigateBack.invoke() }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back Arrow",
+                    tint = if (isSystemInDarkTheme()) Color.White else Color.Black,
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = if (isSystemInDarkTheme())
+                Color(0xFF1B1B1F)
+            else
+                Color(0xFFFEFBFF)
+        ),
+    )
+}
