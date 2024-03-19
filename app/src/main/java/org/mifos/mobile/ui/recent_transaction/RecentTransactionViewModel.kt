8736894c0bf637c7c9007c7935ab2d8a935abc03
Pratyush Singh <aries.pratyush@gmail.com>
2024-03-19 21:16:53 +0530

    refactor #2469: migrated recent transaction to compose

--- app/src/main/java/org/mifos/mobile/ui/recent_transaction/RecentTransactionViewModel.kt
+++ app/src/main/java/org/mifos/mobile/ui/recent_transaction/RecentTransactionViewModel.kt
@@ -1,10 +1,11 @@
-package org.mifos.mobile.viewModels
+package org.mifos.mobile.ui.recent_transaction
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import org.mifos.mobile.R
@@ -22,6 +23,15 @@ class RecentTransactionViewModel @Inject constructor(private val recentTransacti
     private val _recentTransactionUiState =
         MutableStateFlow<RecentTransactionUiState>(RecentTransactionUiState.Initial)
     val recentTransactionUiState: StateFlow<RecentTransactionUiState> = _recentTransactionUiState
+
+    private val _isRefreshing = MutableStateFlow(false)
+    val isRefreshing = _isRefreshing.asStateFlow()
+
+    fun refreshTransactionList() {
+        _isRefreshing.value = true
+        loadRecentTransactions(loadmore, limit)
+        _isRefreshing.value = false
+    }
 
     fun loadRecentTransactions(loadmore: Boolean, offset: Int) {
         this.loadmore = loadmore
