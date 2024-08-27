6e6611d3f07504d0647916b30e89f98d8bf53045
Sk Niyaj Ali <niyaj639@gmail.com>
2024-08-27 19:15:05 +0530

    Initial Detekt, Ktlint and Spotless Setup for Static Analysis (#2671)

---
+++ spotless/copyright.kts
@@ -0,0 +1,9 @@
+/*
+ * Copyright $YEAR Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/mobile-mobile/blob/master/LICENSE.md
+ */
