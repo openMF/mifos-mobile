61643ac0e94809843ca899ec00615472bf8d0e77
Sk Niyaj Ali <niyaj639@gmail.com>
2024-09-01 07:16:28 +0530

    Refactor- [:feature:home] Apply & Fix Detekt, Ktlint Rules (#2682)

--- feature/home/src/main/java/org/mifos/mobile/feature/home/components/TransferDialog.kt
+++ feature/home/src/main/java/org/mifos/mobile/feature/home/components/TransferDialog.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/mobile-mobile/blob/master/LICENSE.md
+ */
 package org.mifos.mobile.feature.home.components
 
 import androidx.compose.foundation.clickable
@@ -6,26 +15,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import org.mifos.mobile.core.designsystem.theme.MifosMobileTheme
+import org.mifos.mobile.core.ui.utils.DevicePreviews
 import org.mifos.mobile.feature.home.R
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TransferDialog(
+internal fun TransferDialog(
     onDismissRequest: () -> Unit,
     navigateToTransfer: () -> Unit,
-    navigateToThirdPartyTransfer: () -> Unit
+    navigateToThirdPartyTransfer: () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    Dialog(onDismissRequest = onDismissRequest) {
+    BasicAlertDialog(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+    ) {
         Card {
             Column(
                 modifier = Modifier
@@ -38,7 +52,7 @@ fun TransferDialog(
                         .fillMaxWidth()
                         .clickable {
                             navigateToTransfer()
-                        }
+                        },
                 )
                 Spacer(modifier = Modifier.height(20.dp))
                 HorizontalDivider()
@@ -49,22 +63,21 @@ fun TransferDialog(
                         .fillMaxWidth()
                         .clickable {
                             navigateToThirdPartyTransfer()
-                        }
+                        },
                 )
             }
         }
     }
 }
 
-
-@Preview(showBackground = true)
+@DevicePreviews
 @Composable
-fun TransferDialogPreview() {
+private fun TransferDialogPreview() {
     MifosMobileTheme {
         TransferDialog(
             onDismissRequest = {},
             navigateToTransfer = {},
-            navigateToThirdPartyTransfer = {}
+            navigateToThirdPartyTransfer = {},
         )
     }
 }
--- feature/home/src/main/java/org/mifos/mobile/feature/home/utils/ImageUtil.kt
+++ feature/home/src/main/java/org/mifos/mobile/feature/home/utils/ImageUtil.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/mobile-mobile/blob/master/LICENSE.md
+ */
 package org.mifos.mobile.feature.home.utils
 
 import android.graphics.Bitmap
@@ -7,130 +16,126 @@ import android.graphics.Matrix
 import android.graphics.Paint
 import android.util.Log
 
-/**
- * Created by dilpreet on 10/8/17.
- */
-class ImageUtil {
-    // Default width and height
-    fun compressImage(decodedBytes: ByteArray): Bitmap? {
-        return compress(decodedBytes, 816.0f, 612.0f)
+internal object ImageUtil {
+    private const val DEFAULT_MAX_WIDTH = 816f
+    private const val DEFAULT_MAX_HEIGHT = 612f
+
+    fun compressImage(
+        decodedBytes: ByteArray,
+        maxWidth: Float = DEFAULT_MAX_WIDTH,
+        maxHeight: Float = DEFAULT_MAX_HEIGHT,
+    ): Bitmap {
+        val options = BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+        }
+
+        BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size, options)
+
+        val (actualWidth, actualHeight) = calculateActualDimensions(options, maxWidth, maxHeight)
+
+        options.apply {
+            inJustDecodeBounds = false
+            inSampleSize = calculateInSampleSize(this, actualWidth, actualHeight)
+            inTempStorage = ByteArray(16 * 1024)
+        }
+
+        val bmp = try {
+            BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size, options)
+        } catch (e: OutOfMemoryError) {
+            Log.e(this::class.java.simpleName, "OutOfMemoryError while decoding bitmap", e)
+            return Bitmap.createBitmap(
+                1,
+                1,
+                Bitmap.Config.ARGB_8888,
+            ) // Return a 1x1 bitmap as fallback
+        }
+
+        return try {
+            createScaledBitmap(bmp, actualWidth, actualHeight, options)
+        } catch (e: OutOfMemoryError) {
+            Log.e(this::class.java.simpleName, "OutOfMemoryError while scaling bitmap", e)
+            bmp // Return the original bitmap if scaling fails
+        }
     }
+}
 
-    fun compressImage(decodedBytes: ByteArray, maxHeight: Float, maxWidth: Float): Bitmap? {
-        return compress(decodedBytes, maxHeight, maxWidth)
-    }
+private fun calculateActualDimensions(
+    options: BitmapFactory.Options,
+    maxWidth: Float,
+    maxHeight: Float,
+): Pair<Int, Int> {
+    var actualWidth = options.outWidth
+    var actualHeight = options.outHeight
+    val imgRatio = actualWidth.toFloat() / actualHeight
+    val maxRatio = maxWidth / maxHeight
 
-    private fun compress(decodedBytes: ByteArray, maxHeight: Float, maxWidth: Float): Bitmap? {
-        var scaledBitmap: Bitmap? = null
-        val options = BitmapFactory.Options()
-
-//      by setting this field as true, the actual bitmap pixels are not loaded in the memory.
-//      Just the bounds are loaded. If
-//      you try the use the bitmap here, you will get null.
-        options.inJustDecodeBounds = true
-        var bmp = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size, options)
-        var actualHeight = options.outHeight
-        var actualWidth = options.outWidth
-        var imgRatio = actualWidth / actualHeight.toFloat()
-        val maxRatio = maxWidth / maxHeight
-
-//      width and height values are set maintaining the aspect ratio of the image
-        if (actualHeight > maxHeight || actualWidth > maxWidth) {
-            if (imgRatio < maxRatio) {
-                imgRatio = maxHeight / actualHeight
-                actualWidth = (imgRatio * actualWidth).toInt()
+    if (actualHeight > maxHeight || actualWidth > maxWidth) {
+        when {
+            imgRatio < maxRatio -> {
                 actualHeight = maxHeight.toInt()
-            } else if (imgRatio > maxRatio) {
-                imgRatio = maxWidth / actualWidth
-                actualHeight = (imgRatio * actualHeight).toInt()
+                actualWidth = (maxHeight * imgRatio).toInt()
+            }
+
+            imgRatio > maxRatio -> {
                 actualWidth = maxWidth.toInt()
-            } else {
+                actualHeight = (maxWidth / imgRatio).toInt()
+            }
+
+            else -> {
                 actualHeight = maxHeight.toInt()
                 actualWidth = maxWidth.toInt()
             }
         }
+    }
 
-//      setting inSampleSize value allows to load a scaled down version of the original image
-        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight)
+    return Pair(actualWidth, actualHeight)
+}
 
-//      inJustDecodeBounds set to false to load the actual bitmap
-        options.inJustDecodeBounds = false
+private fun calculateInSampleSize(
+    options: BitmapFactory.Options,
+    reqWidth: Int,
+    reqHeight: Int,
+): Int {
+    val (height, width) = options.run { outHeight to outWidth }
+    var inSampleSize = 1
 
-//      this options allow android to claim the bitmap memory if it runs low on memory
-        options.inPurgeable = true
-        options.inInputShareable = true
-        options.inTempStorage = ByteArray(16 * 1024)
-        try {
-            bmp = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size, options)
-        } catch (exception: OutOfMemoryError) {
-            Log.e(ImageUtil::class.java.name, exception.toString())
+    if (height > reqHeight || width > reqWidth) {
+        val halfHeight = height / 2
+        val halfWidth = width / 2
+
+        while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+            inSampleSize *= 2
         }
-        try {
-            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888)
-        } catch (exception: OutOfMemoryError) {
-            Log.e(ImageUtil::class.java.name, exception.toString())
-        }
-        val ratioX = actualWidth / options.outWidth.toFloat()
-        val ratioY = actualHeight / options.outHeight.toFloat()
-        val middleX = actualWidth / 2.0f
-        val middleY = actualHeight / 2.0f
-        val scaleMatrix = Matrix()
-        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY)
-        val canvas = Canvas(scaledBitmap!!)
-        canvas.setMatrix(scaleMatrix)
-        canvas.drawBitmap(
+    }
+
+    return inSampleSize
+}
+
+private fun createScaledBitmap(
+    bmp: Bitmap,
+    actualWidth: Int,
+    actualHeight: Int,
+    options: BitmapFactory.Options,
+): Bitmap {
+    val scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888)
+    val ratioX = actualWidth / options.outWidth.toFloat()
+    val ratioY = actualHeight / options.outHeight.toFloat()
+    val middleX = actualWidth / 2f
+    val middleY = actualHeight / 2f
+
+    val scaleMatrix = Matrix().apply {
+        setScale(ratioX, ratioY, middleX, middleY)
+    }
+
+    Canvas(scaledBitmap).apply {
+        setMatrix(scaleMatrix)
+        drawBitmap(
             bmp,
             middleX - bmp.width / 2,
             middleY - bmp.height / 2,
             Paint(Paint.FILTER_BITMAP_FLAG),
         )
-        scaledBitmap = Bitmap.createBitmap(
-            scaledBitmap,
-            0,
-            0,
-            scaledBitmap.width,
-            scaledBitmap.height,
-            null,
-            true,
-        )
-        return scaledBitmap
     }
 
-    private fun calculateInSampleSize(
-        options: BitmapFactory.Options,
-        reqWidth: Int,
-        reqHeight: Int,
-    ): Int {
-        val height = options.outHeight
-        val width = options.outWidth
-        var inSampleSize = 1
-        if (height > reqHeight || width > reqWidth) {
-            val heightRatio = Math.round(height.toFloat() / reqHeight.toFloat())
-            val widthRatio = Math.round(width.toFloat() / reqWidth.toFloat())
-            inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
-        }
-        val totalPixels = width * height.toFloat()
-        val totalReqPixelsCap = reqWidth * reqHeight * 2.toFloat()
-        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
-            inSampleSize++
-        }
-        return inSampleSize
-    }
-
-    companion object {
-        /**
-         * Reference : https://developer.android.com/topic/performance/graphics/load-bitmap.html
-         * And for scaling :
-         * https://stackoverflow.com/questions/8722359/scale-rotate-bitmap-using-matrix-in-android/8722592#8722592
-         */
-        @JvmStatic
-        var instance: ImageUtil? = null
-            get() {
-                if (field == null) {
-                    field = ImageUtil()
-                }
-                return field
-            }
-            private set
-    }
+    return scaledBitmap
 }
