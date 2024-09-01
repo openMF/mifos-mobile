f4a394a4f089d4c0c339411af94870a20fceb80b
Sk Niyaj Ali <niyaj639@gmail.com>
2024-09-01 07:16:12 +0530

    Refactor- [:feature:location] Apply & Fix Detekt, Ktlint Rules (#2684)

--- feature/location/src/main/java/org/mifos/mobile/feature/location/LocationScreen.kt
+++ feature/location/src/main/java/org/mifos/mobile/feature/location/LocationScreen.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/mobile-mobile/blob/master/LICENSE.md
+ */
 package org.mifos.mobile.feature.location
 
 import androidx.compose.foundation.layout.Column
@@ -10,7 +19,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -19,23 +27,26 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import org.mifos.mobile.core.designsystem.theme.MifosMobileTheme
+import org.mifos.mobile.core.ui.utils.DevicePreviews
 
 @Composable
-fun LocationsScreen() {
+internal fun LocationsScreen(
+    modifier: Modifier = Modifier,
+) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
     ) {
         Text(
             text = stringResource(id = R.string.mifos_initiative),
             color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.bodyLarge
+            style = MaterialTheme.typography.bodyLarge,
         )
         Text(
             text = stringResource(id = R.string.mifos_location),
             color = MaterialTheme.colorScheme.onBackground,
-            style = MaterialTheme.typography.bodyMedium
+            style = MaterialTheme.typography.bodyMedium,
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -46,20 +57,21 @@ fun LocationsScreen() {
         }
 
         GoogleMap(
-            modifier = Modifier.fillMaxSize(), cameraPositionState = cameraPositionState
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
         ) {
             Marker(
                 state = MarkerState(position = headquarterLatLng),
                 title = stringResource(id = R.string.mifos_initiative),
-                snippet = stringResource(id = R.string.mifos_location)
+                snippet = stringResource(id = R.string.mifos_location),
             )
         }
     }
 }
 
 @Composable
-@Preview(showSystemUi = true)
-fun PreviewLocationsScreen() {
+@DevicePreviews
+private fun PreviewLocationsScreen() {
     MifosMobileTheme {
         LocationsScreen()
     }
