a4ba1a2697392c6b2279cbb5ac14fa4966542029
Sk Niyaj Ali <niyaj639@gmail.com>
2024-09-02 20:54:55 +0530

    Refactor- [:feature:third-party-transfer] Apply & Fix Detekt, Ktlint Rules (#2690)
    Jira Tasks - [MM-78](https://mifosforge.jira.com/browse/MM-78)

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -17,13 +17,5 @@ android {
 }
 
 dependencies {
-    implementation(projects.core.ui)
-    implementation(projects.core.common)
-    implementation(projects.core.model)
-    implementation(projects.core.data)
     implementation(libs.androidx.appcompat)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
