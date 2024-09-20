27c34661d91b4f9613fb4c9eb7bab5f0f7cff974
Saifuddin <adenwala32@gmail.com>
2024-09-20 19:19:36 +0530

    Status bar theme fixed

--- core/designsystem/src/main/kotlin/org/mifos/mobile/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/org/mifos/mobile/core/designsystem/theme/Theme.kt
@@ -9,12 +9,17 @@
  */
 package org.mifos.mobile.core.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightThemeColors = lightColorScheme(
     primary = LightPrimary,
@@ -48,6 +53,15 @@ fun MifosMobileTheme(
     val colors = when {
         useDarkTheme -> DarkThemeColors
         else -> LightThemeColors
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !useDarkTheme
+        }
     }
 
     MaterialTheme(
