a34e8e41f0b4b516bc5ab599274f491947e75b35
Hekmatullah <hekmatullah.aminullah2@gmail.com>
2025-02-23 18:42:21 +0000

    feat: `(feature:loan-account)` Migrate loan-account module to CMP (#2775)

---
+++ feature/loan-account/src/commonMain/kotlin/org/mifos/mobile/feature/loanaccount/utils/AccountState.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/mobile-mobile/blob/master/LICENSE.md
+ */
+package org.mifos.mobile.feature.loanaccount.utils
+
+import org.mifos.mobile.core.model.entity.accounts.loan.LoanAccount
+
+sealed class AccountState {
+    data object Error : AccountState()
+    data object Loading : AccountState()
+    data object Empty : AccountState()
+    data class Success(val filteredLoanAccounts: List<LoanAccount>) : AccountState()
+}
