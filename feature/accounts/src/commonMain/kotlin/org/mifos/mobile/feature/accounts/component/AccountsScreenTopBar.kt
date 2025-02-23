c673ec38555617412c36775b0cc7da10cff78b43
Hekmatullah <hekmatullah.aminullah2@gmail.com>
2025-02-23 18:44:19 +0000

    Add missing imports and removed unnecessary properties from Composables. (#2781)

--- feature/accounts/src/commonMain/kotlin/org/mifos/mobile/feature/accounts/component/AccountsScreenTopBar.kt
+++ feature/accounts/src/commonMain/kotlin/org/mifos/mobile/feature/accounts/component/AccountsScreenTopBar.kt
@@ -9,6 +9,32 @@
  */
 package org.mifos.mobile.feature.accounts.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import org.mifos.mobile.core.designsystem.component.MifosSearchTextField
+import org.mifos.mobile.core.designsystem.icon.MifosIcons
+
 @Composable
 internal fun AccountsScreenTopBar(
     navigateBack: () -> Unit,
