6663b544ab2a5554b074e1702519a2c98cacf203
Pronay sarker <pronaycoding@gmail.com>
2025-03-09 20:38:12 +0600

    fix

--- cmp-navigation/src/commonMain/kotlin/cmp/navigation/di/KoinModules.kt
+++ cmp-navigation/src/commonMain/kotlin/cmp/navigation/di/KoinModules.kt
@@ -27,6 +27,7 @@ import org.mifos.mobile.feature.loanaccount.di.loanAccountModule
 import org.mifos.mobile.feature.savingsaccount.di.savingsAccountModule
 import org.mifos.mobile.feature.shareaccount.di.shareAccountModule
 import org.mifos.mobile.feature.third.party.transfer.di.ThirdPartyTransferModule
+import org.mifos.mobile.feature.transfer.process.di.TransferProcessModule
 import org.mifos.mobile.feature.update.password.di.updatePasswordModule
 
 object KoinModules {
@@ -56,6 +57,7 @@ object KoinModules {
             shareAccountModule,
             LoanModule,
             ChargeModule,
+            TransferProcessModule,
             ThirdPartyTransferModule,
             updatePasswordModule,
         )
