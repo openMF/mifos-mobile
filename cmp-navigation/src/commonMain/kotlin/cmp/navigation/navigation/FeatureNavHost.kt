be917be905ad2afe717c5919cda0ea0652b3f572
revanthkumarJ <jrevanth101@gmail.com>
2025-03-14 00:29:31 +0530

    updated

--- cmp-navigation/src/commonMain/kotlin/cmp/navigation/navigation/FeatureNavHost.kt
+++ cmp-navigation/src/commonMain/kotlin/cmp/navigation/navigation/FeatureNavHost.kt
@@ -41,13 +41,13 @@ import org.mifos.mobile.feature.home.navigation.navigateToHomeScreen
 import org.mifos.mobile.feature.loan.navigation.loanNavGraph
 import org.mifos.mobile.feature.loan.navigation.navigateToLoanApplication
 import org.mifos.mobile.feature.loan.navigation.navigateToLoanDetailScreen
-import org.mifos.mobile.feature.savings.navigation.navigateToSavingsApplicationScreen
-import org.mifos.mobile.feature.savings.navigation.navigateToSavingsDetailScreen
-import org.mifos.mobile.feature.savings.navigation.savingsNavGraph
 import org.mifos.mobile.feature.qr.navigation.navigateToQrDisplayScreen
 import org.mifos.mobile.feature.qr.navigation.navigateToQrImportScreen
 import org.mifos.mobile.feature.qr.navigation.navigateToQrReaderScreen
 import org.mifos.mobile.feature.qr.navigation.qrNavGraph
+import org.mifos.mobile.feature.savings.navigation.navigateToSavingsApplicationScreen
+import org.mifos.mobile.feature.savings.navigation.navigateToSavingsDetailScreen
+import org.mifos.mobile.feature.savings.navigation.savingsNavGraph
 import org.mifos.mobile.feature.settings.navigation.navigateToSettings
 import org.mifos.mobile.feature.settings.navigation.settingsNavGraph
 import org.mifos.mobile.feature.third.party.transfer.navigation.navigateToThirdPartyTransfer
