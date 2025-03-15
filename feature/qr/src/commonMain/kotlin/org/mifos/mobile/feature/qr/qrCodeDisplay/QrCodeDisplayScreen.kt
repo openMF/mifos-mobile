7ca85b11e1fe9fb3d97dc272e3eb2a5552320226
Nagarjuna <nagarjunabanda4@gmail.com>
2025-03-15 20:29:31 +0530

    fix: spotless error

--- feature/qr/src/commonMain/kotlin/org/mifos/mobile/feature/qr/qrCodeDisplay/QrCodeDisplayScreen.kt
+++ feature/qr/src/commonMain/kotlin/org/mifos/mobile/feature/qr/qrCodeDisplay/QrCodeDisplayScreen.kt
@@ -170,7 +170,7 @@ private fun QrCodeDisplayContent(
             Text(
                 text = "Mifos Mobile",
                 style = MaterialTheme.typography.titleLarge,
-                color = Color.Black
+                color = Color.Black,
             )
 
             Image(
