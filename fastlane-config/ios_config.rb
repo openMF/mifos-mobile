2b077cad560672f7638d32cff8e9071c4d5cc622
Hekmatullah <hekmatullah.aminullah2@gmail.com>
2025-05-15 20:50:06 +0100

    Changed testers group name used for app distribution

--- fastlane-config/ios_config.rb
+++ fastlane-config/ios_config.rb
@@ -3,7 +3,7 @@ module FastlaneConfig
     FIREBASE_CONFIG = {
       firebase_app_id: "1:728434912738:ios:shjhsa78392shja",
       firebase_service_creds_file: "secrets/firebaseAppDistributionServiceCredentialsFile.json",
-      firebase_groups: "kmp-project-template-testers"
+      firebase_groups: "mifos-mobile-apps"
     }
 
     BUILD_CONFIG = {
