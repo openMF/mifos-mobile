0f8d00abd14bad4d73ede0a1b3172822691a0ea5
Nagarjuna <nagarjunabanda4@gmail.com>
2025-06-27 21:13:11 +0530

    fix: web checks

--- core/designsystem/src/commonMain/kotlin/org/mifos/mobile/core/designsystem/component/MifosCard.kt
+++ core/designsystem/src/commonMain/kotlin/org/mifos/mobile/core/designsystem/component/MifosCard.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -268,6 +269,7 @@ fun MifosUploadedStateCard(
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun MifosUploadedCardContent(
     removeText: String,
