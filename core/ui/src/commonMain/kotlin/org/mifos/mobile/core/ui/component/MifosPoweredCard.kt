60b1434c1f254fa4f754288f26d3f949f9bd68fd
Nagarjuna <nagarjunabanda4@gmail.com>
2025-06-29 14:21:35 +0530

    fix: checks

--- core/ui/src/commonMain/kotlin/org/mifos/mobile/core/ui/component/MifosPoweredCard.kt
+++ core/ui/src/commonMain/kotlin/org/mifos/mobile/core/ui/component/MifosPoweredCard.kt
@@ -39,9 +39,9 @@ import org.mifos.mobile.core.designsystem.theme.MifosTypography
 
 @Composable
 fun MifosPoweredCard(
+    modifier: Modifier = Modifier,
     text: String? = stringResource(Res.string.powered_by),
     icon: DrawableResource? = Res.drawable.ic_icon_powered_logo,
-    modifier: Modifier = Modifier,
 ) {
     Box(
         modifier = modifier
