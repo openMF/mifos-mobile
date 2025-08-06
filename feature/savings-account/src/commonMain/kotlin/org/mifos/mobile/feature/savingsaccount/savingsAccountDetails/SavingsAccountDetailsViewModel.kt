c4d3e10684ad1b6ec241467de84d4a1f35c4b688
revanthkumarJ <jrevanth101@gmail.com>
2025-08-06 11:24:54 +0530

    updated

--- feature/savings-account/src/commonMain/kotlin/org/mifos/mobile/feature/savingsaccount/savingsAccountDetails/SavingsAccountDetailsViewModel.kt
+++ feature/savings-account/src/commonMain/kotlin/org/mifos/mobile/feature/savingsaccount/savingsAccountDetails/SavingsAccountDetailsViewModel.kt
@@ -149,7 +149,7 @@ internal class SavingsAccountDetailsViewModel(
                 clientName = state.clientName.toString(),
                 accountNumber = state.accountNumber.toString(),
                 accountType = AccountType(
-                    id = 1,
+                    id = 2,
                     code = "accountType.savings",
                     value = "Savings Account",
                 ),
