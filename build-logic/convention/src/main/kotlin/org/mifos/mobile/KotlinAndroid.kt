d3d61125915e0a81f4513be45e48dda6b73a64bb
Hekmatullah <hekmatullah.aminullah2@gmail.com>
2025-10-04 08:21:41 +0100

    set JVM target to 21

--- build-logic/convention/src/main/kotlin/org/mifos/mobile/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/org/mifos/mobile/KotlinAndroid.kt
@@ -62,8 +62,8 @@ private fun Project.configureKotlin() {
     // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
-            // Set JVM target to 17
-            jvmTarget = JvmTarget.JVM_17
+            // Set JVM target to 21
+            jvmTarget = JvmTarget.JVM_21
             // Treat all Kotlin warnings as errors (disabled by default)
             // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
             val warningsAsErrors: String? by project
