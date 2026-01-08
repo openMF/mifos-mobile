d65ed3cae7d1d4b466ec3f4d796e4fd59bbe5b83
mark <mark.rizkalla1@gmail.com>
2026-01-08 06:06:51 +0200

    fix(thirdPartyTransfer): Filter fromAccounts to only show savings accounts

--- feature/third-party-transfer/src/commonMain/kotlin/org/mifos/mobile/feature/third/party/transfer/thirdPartyTransfer/TptViewModel.kt
+++ feature/third-party-transfer/src/commonMain/kotlin/org/mifos/mobile/feature/third/party/transfer/thirdPartyTransfer/TptViewModel.kt
@@ -28,6 +28,7 @@ import org.mifos.mobile.core.datastore.UserPreferencesRepository
 import org.mifos.mobile.core.model.entity.payload.ReviewTransferPayload
 import org.mifos.mobile.core.model.entity.templates.account.AccountOption
 import org.mifos.mobile.core.model.entity.templates.account.AccountOptionsTemplate
+import org.mifos.mobile.core.model.enums.AccountType
 import org.mifos.mobile.core.ui.utils.BaseViewModel
 import org.mifos.mobile.core.ui.utils.ScreenUiState
 
@@ -160,11 +161,12 @@ internal class TptViewModel(
      */
     private fun handleFromAccountChange(fromAccount: String) {
         val fromAccountSelected = state.accountOptionsTemplate.fromAccountOptions
+            .filter { it.accountType?.value == AccountType.SAVINGS.value }
             .find { it.accountNo == fromAccount }
 
-        val toAccounts = state.accountOptionsTemplate.toAccountOptions.filter {
-            it.accountNo != fromAccount
-        }
+        val toAccounts = state.accountOptionsTemplate.toAccountOptions
+            .filter { it.accountNo != fromAccount }
+
         updateState {
             it.copy(
                 fromAccount = fromAccountSelected,
@@ -187,9 +189,11 @@ internal class TptViewModel(
         val toAccountSelected = state.accountOptionsTemplate.toAccountOptions
             .find { it.accountNo == toAccount }
 
-        val fromAccounts = state.accountOptionsTemplate.fromAccountOptions.filter {
-            it.accountNo != toAccount
-        }
+        val fromAccounts = state.accountOptionsTemplate.fromAccountOptions
+            .filter {
+                it.accountType?.value == AccountType.SAVINGS.value &&
+                    it.accountNo != toAccount
+            }
 
         updateState {
             it.copy(
@@ -417,10 +421,16 @@ internal class TptViewModel(
             DataState.Loading -> showLoading()
 
             is DataState.Success -> {
+                val template = dataState.data
+
+                val savingsFromAccounts = template.fromAccountOptions.filter {
+                    it.accountType?.value == AccountType.SAVINGS.value
+                }
+
                 updateState {
                     it.copy(
                         accountOptionsTemplate = dataState.data,
-                        fromAccountOptions = dataState.data.fromAccountOptions,
+                        fromAccountOptions = savingsFromAccounts,
                         toAccountOptions = dataState.data.toAccountOptions,
                         uiState = ScreenUiState.Success,
                     )
