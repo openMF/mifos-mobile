7e167dd858b9c23b9e9006c002d055578568062b
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-02-16 10:20:30 +0530

    MM-395: Fix spotless formatting

--- feature/home/src/commonMain/kotlin/org/mifos/mobile/feature/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/org/mifos/mobile/feature/home/HomeScreen.kt
@@ -11,7 +11,6 @@ package org.mifos.mobile.feature.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
