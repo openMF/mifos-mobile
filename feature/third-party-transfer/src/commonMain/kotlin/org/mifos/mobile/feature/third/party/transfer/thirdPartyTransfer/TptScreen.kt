b5c643763a1f8d58d7c97655fe95665161017f63
wizcoderr <ssayam2000@gmail.com>
2026-02-20 00:19:20 +0530

    some fixes

--- feature/third-party-transfer/src/commonMain/kotlin/org/mifos/mobile/feature/third/party/transfer/thirdPartyTransfer/TptScreen.kt
+++ feature/third-party-transfer/src/commonMain/kotlin/org/mifos/mobile/feature/third/party/transfer/thirdPartyTransfer/TptScreen.kt
@@ -12,7 +12,6 @@ package org.mifos.mobile.feature.third.party.transfer.thirdPartyTransfer
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
