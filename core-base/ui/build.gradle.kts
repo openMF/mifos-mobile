aea6aa72514ca5096d3270ff3fb4da718a39e09f
Ambarish Manna <amanna1041@gmail.com>
2026-02-27 20:23:00 +0530

    fix: fixed addressed changes

--- core-base/ui/build.gradle.kts
+++ core-base/ui/build.gradle.kts
@@ -8,16 +8,6 @@
  * See https://github.com/openMF/mobile-mobile/blob/master/LICENSE.md
  */
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-
-/*
- * Copyright 2025 Mifos Initiative
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * See https://github.com/openMF/kmp-project-template/blob/main/LICENSE
- */
 plugins {
     alias(libs.plugins.kmp.core.base.library.convention)
     alias(libs.plugins.jetbrainsCompose)
