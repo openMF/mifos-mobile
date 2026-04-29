98a4884fcd7afcfa48dd22b36ad6977cfec09cc7
Ayush Patel <ayushpatel2731@gmail.com>
2026-04-29 15:28:45 +0530

    fix(security): enforce HTTPS-only connections (MM-557)

--- core/network/src/commonMain/kotlin/org/mifos/mobile/core/network/utils/BaseURL.kt
+++ core/network/src/commonMain/kotlin/org/mifos/mobile/core/network/utils/BaseURL.kt
@@ -16,10 +16,6 @@ class BaseURL {
     val defaultBaseUrl: String
         get() = PROTOCOL_HTTPS + API_ENDPOINT
 
-    fun getUrl(endpoint: String): String {
-        return endpoint + API_PATH
-    }
-
     companion object {
         const val API_ENDPOINT = "tt.mifos.community"
         const val API_PATH = "/fineract-provider/api/v1/self/"
